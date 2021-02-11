bf3d8058eeff46f3996d5a3466540a1783fc6e1d
Aleksei Lebedev <LastDragon@yandex.ru>
2021-02-11 10:25:25 +0400

    `RedirectIfAuthenticated` middleware renamed to `Guest` and will throw `AccessDeniedHttpException` for users.

--- app/Http/Middleware/Guest.php
+++ app/Http/Middleware/Guest.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
-use function redirect;
-
-class RedirectIfAuthenticated {
+/**
+ * Only guests allowed.
+ */
+class Guest {
     /**
      * Handle an incoming request.
      */
@@ -18,7 +19,7 @@ class RedirectIfAuthenticated {
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                throw new AccessDeniedHttpException();
             }
         }
 
