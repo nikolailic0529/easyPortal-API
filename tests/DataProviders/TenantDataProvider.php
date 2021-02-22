349aeefcffa62a237e21cf895a5cd9135108f4df
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-02-22 16:05:50 +0400

    `TenantDataProvider` will not check Content-Type of response.

--- tests/DataProviders/TenantDataProvider.php
+++ tests/DataProviders/TenantDataProvider.php
@@ -3,17 +3,17 @@
 namespace Tests\DataProviders;
 
 use App\Models\Organization;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\StatusCodes\NotFound;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\ExpectedFinal;
 use LastDragon_ru\LaraASP\Testing\Providers\Unknown;
-use LastDragon_ru\LaraASP\Testing\Responses\Laravel\Json\NotFoundResponse;
 use Tests\TestCase;
 
 class TenantDataProvider extends ArrayDataProvider {
     public function __construct() {
         parent::__construct([
             'no tenant' => [
-                new ExpectedFinal(new NotFoundResponse()),
+                new ExpectedFinal(new NotFound()),
                 static function (): ?Organization {
                     return null;
                 },
