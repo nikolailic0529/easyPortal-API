9e40d6272c2993b014ee4c1ee3dffb41491e604e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-02-24 15:57:43 +0400

    Country model docblock.

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Country.
+ *
+ * @property string                       $id
+ * @property string                       $code
+ * @property string                       $name
+ * @property \Carbon\CarbonImmutable      $created_at
+ * @property \Carbon\CarbonImmutable      $updated_at
+ * @property \Carbon\CarbonImmutable|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Country extends Model {
     use HasFactory;
 
