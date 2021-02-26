cb8068e896874a1a4488d3dc78e7308b9a124e9b
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-02-26 17:07:00 +0400

    `Model::getMorphClass()` will throws exception if model not in Morph Map.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use App\Models\Concerns\UuidAsPrimaryKey;
 use LastDragon_ru\LaraASP\Eloquent\Model as LaraASPModel;
+use LogicException;
+
+use function sprintf;
 
 abstract class Model extends LaraASPModel {
     use UuidAsPrimaryKey;
@@ -36,4 +39,24 @@ abstract class Model extends LaraASPModel {
      * @var array<string>
      */
     protected $casts = self::CASTS;
+
+    public function getMorphClass(): string {
+        $class = parent::getMorphClass();
+
+        if ($class === static::class) {
+            /**
+             * Storing class names in a database is a very bad idea. You should
+             * add a name for the model into MorphMap.
+             *
+             * @see \App\Providers\AppServiceProvider::boot()
+             */
+
+            throw new LogicException(sprintf(
+                'Please add morph name for `%s` model.',
+                static::class,
+            ));
+        }
+
+        return $class;
+    }
 }
