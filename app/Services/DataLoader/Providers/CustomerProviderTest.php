df478f8c5efdce42cdccc78d283c6f51b994ed05
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-03-01 10:06:35 +0400

    CustomerProvider.

---
+++ app/Services/DataLoader/Providers/CustomerProviderTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Providers;
+
+use App\Models\Customer;
+use Closure;
+use LastDragon_ru\LaraASP\Testing\Database\WithQueryLog;
+use Mockery;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\Services\DataLoader\Providers\CustomerProvider
+ */
+class CustomerProviderTest extends TestCase {
+    use WithQueryLog;
+
+    /**
+     * @covers ::get
+     */
+    public function testGet(): void {
+        // Prepare
+        $factory = static function (): Customer {
+            return Customer::factory()->make();
+        };
+
+        $a = Customer::factory()->create();
+        $b = Customer::factory()->create();
+
+        // Run
+        $provider = $this->app->make(CustomerProvider::class);
+        $actual   = $provider->get($a->getKey(), $factory);
+
+        $this->flushQueryLog();
+
+        // Basic
+        $this->assertNotNull($actual);
+        $this->assertEquals($a->getKey(), $actual->getKey());
+
+        // Second call should return same instance
+        $this->assertSame($actual, $provider->get($a->getKey(), $factory));
+        $this->assertSame($actual, $provider->get(" {$a->getKey()} ", $factory));
+
+        $this->assertCount(0, $this->getQueryLog());
+
+        $this->assertNotSame($actual, $provider->get($b->getKey(), $factory));
+        $this->assertCount(1, $this->getQueryLog());
+        $this->flushQueryLog();
+
+        // If value not found the new object should be created
+        $uuid    = $this->faker->uuid;
+        $spy     = Mockery::spy(static function () use ($uuid): Customer {
+            return Customer::factory()->make([
+                'id'        => $uuid,
+                'type_id'   => $uuid,
+                'status_id' => $uuid,
+            ]);
+        });
+        $created = $provider->get($uuid, Closure::fromCallable($spy));
+
+        $spy->shouldHaveBeenCalled();
+
+        $this->assertNotNull($created);
+        $this->assertEquals($uuid, $created->getKey());
+        $this->assertCount(1, $this->getQueryLog());
+
+        $this->flushQueryLog();
+
+        // The created object should be in cache
+        $this->assertSame($created, $provider->get($uuid, $factory));
+        $this->assertCount(0, $this->getQueryLog());
+    }
+}
