27a3d903a2f33a28d9db279f6e735f23f11c0d5d
Mohamed <mohammedosama.amagd@gmail.com>
2021-03-15 10:47:33 +0200

    [GraphQL] New endpoint `locationTypes` (return all possible types for Location)
    Fixes:
    * https://thefas.atlassian.net/browse/EAP-9

---
+++ app/GraphQL/Queries/LocationTypesTest.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Location;
+use App\Models\Type;
+use Closure;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Tests\DataProviders\GraphQL\AnyDataProvider;
+use Tests\DataProviders\TenantDataProvider;
+use Tests\GraphQL\GraphQLSuccess;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\GraphQL\Queries\LocationTypes
+ */
+class LocationTypesTest extends TestCase {
+    /**
+     * @covers ::__invoke
+     * @dataProvider dataProviderInvoke
+     */
+    public function testInvoke(
+        Response $expected,
+        Closure $tenantFactory,
+        Closure $userFactory = null,
+        Closure $typeFactory = null,
+    ): void {
+        // Prepare
+        $this->setUser($userFactory, $this->setTenant($tenantFactory));
+
+        if ($typeFactory) {
+            $typeFactory($this);
+        }
+
+        // Test
+        $this
+            ->graphQL(/** @lang GraphQL */ '{
+                locationTypes {
+                    id
+                    name
+                }
+            }')
+            ->assertThat($expected);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderInvoke(): array {
+        return (new CompositeDataProvider(
+            new TenantDataProvider(),
+            new AnyDataProvider(),
+            new ArrayDataProvider([
+                'ok' => [
+                    new GraphQLSuccess('locationTypes', LocationTypes::class, [
+                        'data' => [
+                            'locationTypes' => [
+                                [
+                                    'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                                    'name' => 'Type1',
+                                ],
+                            ],
+                        ],
+                    ]),
+                    static function (): void {
+                        Type::factory()
+                            ->create([
+                                'id'          => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                                'name'        => 'Type1',
+                                'object_type' => (new Location())->getMorphClass(),
+                            ]);
+                    },
+                ],
+            ]),
+        ))->getData();
+    }
+    // </editor-fold>
+}
