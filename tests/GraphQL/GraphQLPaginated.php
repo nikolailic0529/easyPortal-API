c72d7965716bb97543b415257a13037c4565360c
Mohamed <mohammedosama.amagd@gmail.com>
2021-03-15 13:46:35 +0200

    [GraphQL] New endpoint `assets`

---
+++ tests/GraphQL/GraphQLPaginated.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\GraphQL;
+
+use JsonSerializable;
+use LastDragon_ru\LaraASP\Testing\Responses\Laravel\Json\OkResponse;
+use SplFileInfo;
+use stdClass;
+
+class GraphQLPaginated extends OkResponse {
+    public function __construct(
+        string $root,
+        ?string $schema,
+        SplFileInfo|array|string|stdClass|JsonSerializable|null $content = null,
+    ) {
+        parent::__construct(new SchemaWrapper($this::class, $root, $schema), $content);
+    }
+}
