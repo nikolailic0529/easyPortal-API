2ddd79c920b899dfe473813eb69b5e60eee70f64
Mohamed <mohammedosama.amagd@gmail.com>
2021-03-18 08:13:05 +0200

    Added `oems` endpoint
    Closes:
    - https://thefas.atlassian.net/browse/EAP-39

---
+++ app/GraphQL/Queries/OemsTest.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Oem;
+use Closure;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Tests\DataProviders\GraphQL\AnyDataProvider;
+use Tests\DataProviders\TenantDataProvider;
+use Tests\GraphQL\GraphQLSuccess;
+use Tests\TestCase;
+
+/**
+ * @internal
+ */
+class OemsTest extends TestCase {
+    /**
+     * @dataProvider dataProviderInvoke
+     * @coversNothing
+     */
+    public function testQuery(
+        Response $expected,
+        Closure $tenantFactory,
+        Closure $userFactory = null,
+        Closure $oemsFactory = null,
+    ): void {
+        // Prepare
+        $this->setUser($userFactory, $this->setTenant($tenantFactory));
+
+        if ($oemsFactory) {
+            $oemsFactory($this);
+        }
+
+        // Test
+        $this
+            ->graphQL(/** @lang GraphQL */ '{
+                oems {
+                    id
+                    abbr
+                    name
+                }
+            }')
+            ->assertThat($expected);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderInvoke(): array {
+        return (new CompositeDataProvider(
+            new TenantDataProvider(),
+            new AnyDataProvider(),
+            new ArrayDataProvider([
+                'ok' => [
+                    new GraphQLSuccess('oems', self::class, [
+                        [
+                            'id'   => '439a0a06-d98a-41f0-b8e5-4e5722518e00',
+                            'abbr' => 'abr',
+                            'name' => 'oem1',
+                        ],
+                    ]),
+                    static function (): void {
+                        Oem::factory()->create([
+                            'id'   => '439a0a06-d98a-41f0-b8e5-4e5722518e00',
+                            'abbr' => 'abr',
+                            'name' => 'oem1',
+                        ]);
+                    },
+                ],
+            ]),
+        ))->getData();
+    }
+    // </editor-fold>
+}
