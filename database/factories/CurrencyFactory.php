4a3463edc68910d32044cbc85424146808b1a5dc
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-03-19 11:07:18 +0400

    Currency model.

---
+++ database/factories/CurrencyFactory.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace Database\Factories;
+
+use App\Models\Currency;
+use Illuminate\Support\Facades\Date;
+use LastDragon_ru\LaraASP\Testing\Database\Eloquent\Factories\Factory;
+
+class CurrencyFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
+     *
+     * @var string
+     */
+    protected $model = Currency::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<mixed>
+     */
+    public function definition(): array {
+        return [
+            'id'         => $this->faker->uuid,
+            'code'       => $this->faker->currencyCode,
+            'name'       => $this->faker->currencyCode,
+            'created_at' => Date::now(),
+            'updated_at' => Date::now(),
+            'deleted_at' => null,
+        ];
+    }
+}
