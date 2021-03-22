fd288ad05d6448d8d65b8edf8a8bbf55616838ff
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-03-22 11:23:50 +0400

    New Schema types: AssetDocument, Document, DocumentVendorSpecificField.

---
+++ app/Services/DataLoader/Schema/DocumentVendorSpecificField.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Schema;
+
+class DocumentVendorSpecificField extends Type {
+    public string $vendor;
+    public string $totalNetPrice;
+}
