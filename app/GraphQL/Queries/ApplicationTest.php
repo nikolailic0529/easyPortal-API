bff03b0cb26c4e0fc67f51c6756539eaa4813fa9
Mohamed <mohammedosama.amagd@gmail.com>
2021-04-02 13:14:52 +0200

    New Application query (#60)
    Closes:
    - https://thefas.atlassian.net/browse/EAP-55

---
+++ app/GraphQL/Queries/ApplicationTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Queries;
+
+use Closure;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Tests\DataProviders\GraphQL\AnyDataProvider;
+use Tests\DataProviders\TenantDataProvider;
+use Tests\GraphQL\GraphQLSuccess;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\GraphQL\Queries\ApplicationTest
+ */
+class ApplicationTest extends TestCase {
+    /**
+     * @dataProvider dataProviderQuery
+     */
+    public function testQuery(
+        Response $expected,
+        Closure $tenantFactory,
+        Closure $userFactory = null,
+    ): void {
+        // Prepare
+        $this->setUser($userFactory, $this->setTenant($tenantFactory));
+
+        // Test
+        $this
+            ->graphQL(/** @lang GraphQL */ '
+                {
+                    application {
+                        name
+                        version
+                    }
+                }
+            ')
+            ->assertThat($expected);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderQuery(): array {
+        return (new CompositeDataProvider(
+            new TenantDataProvider(),
+            new AnyDataProvider(),
+            new ArrayDataProvider([
+                'ok' => [
+                    new GraphQLSuccess('application', Application::class, null),
+                ],
+            ]),
+        ))->getData();
+    }
+    // </editor-fold>
+}
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,29 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Composer\InstalledVersions;
-use Illuminate\Config\Repository;
-use Illuminate\Contracts\Foundation\Application;
+use App\GraphQL\Queries\Application as ApplicationQuery;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Foundation\Exceptions\RegisterErrorViewPaths;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-use function file_get_contents;
-use function json_decode;
 
 class IndexController extends Controller {
     /**
      * @return \View|array<string,string>
      */
-    public function index(Application $app, Repository $config, Request $request, Factory $view): View|array {
-        $name     = $config->get('app.name');
-        $package  = json_decode(file_get_contents($app->basePath('composer.json')), true)['name'];
-        $version  = InstalledVersions::getVersion($package);
-        $response = [
-            'name'    => $name,
-            'version' => $version,
-        ];
+    public function index(ApplicationQuery $query, Request $request, Factory $view): View|array {
+        $response = $query(null, []);
 
         if (!$request->expectsJson()) {
             (new RegisterErrorViewPaths())();
