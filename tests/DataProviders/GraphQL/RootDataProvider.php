af8075c72655b5f9a6a7db484eefe69b003c07e3
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-05 17:33:26 +0400

    Added `RootDataProvider` (action allowed only for root)

---
+++ tests/DataProviders/GraphQL/RootDataProvider.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\DataProviders\GraphQL;
+
+use App\Models\Organization;
+use App\Models\User;
+use Illuminate\Contracts\Config\Repository;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\ExpectedFinal;
+use LastDragon_ru\LaraASP\Testing\Providers\Unknown;
+use Tests\GraphQL\GraphQLError;
+use Tests\TestCase;
+
+use function __;
+
+class RootDataProvider extends ArrayDataProvider {
+    public function __construct(string $root) {
+        parent::__construct([
+            'guest is not allowed' => [
+                new ExpectedFinal(new GraphQLError($root, static function (): array {
+                    return [__('errors.unauthenticated')];
+                })),
+                static function (): ?User {
+                    return null;
+                },
+            ],
+            'user is not allowed'  => [
+                new ExpectedFinal(new GraphQLError($root, static function (): array {
+                    return [__('errors.unauthorized')];
+                })),
+                static function (TestCase $test, ?Organization $organization): ?User {
+                    return User::factory()->make([
+                        'organization_id' => $organization,
+                    ]);
+                },
+            ],
+            'root is allowed'      => [
+                new Unknown(),
+                static function (TestCase $test, ?Organization $organization): ?User {
+                    $user = User::factory()->make();
+
+                    $test->app()->make(Repository::class)->set(
+                        'easyportal.root_user_id',
+                        $user->getKey(),
+                    );
+
+                    return $user;
+                },
+            ],
+        ]);
+    }
+}
