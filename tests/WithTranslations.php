db0598009ed2d58f139150c56eae34facf256462
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-07 10:33:07 +0400

    [tests] Added `WithTranslations` trait.

---
+++ tests/WithTranslations.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Tests;
+
+use Closure;
+use Illuminate\Translation\Translator;
+
+/**
+ * @mixin \Tests\TestCase
+ */
+trait WithTranslations {
+    /**
+     * @param \Closure(\Tests\TestCase $test,string $locale, string $fallback):array<string,array<string,string>>|null
+     *      $translations
+     */
+    public function setTranslations(Closure|null $translations): void {
+        $translator   = $this->app->make(Translator::class);
+        $translations = $translations instanceof Closure
+            ? $translations($this, $translator->getLocale(), $translator->getFallback())
+            : [];
+
+        foreach ((array) $translations as $locale => $lines) {
+            $translator->addLines($lines, $locale);
+        }
+    }
+}
