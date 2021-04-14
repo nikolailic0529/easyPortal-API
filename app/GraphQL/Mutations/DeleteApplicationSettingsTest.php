49dd035593eed9ec4a7750275ae0daf0c9694f97
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-14 10:36:46 +0400

    New mutation: deleteApplicationSettings

---
+++ app/GraphQL/Mutations/DeleteApplicationSettingsTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Services\Settings\Storage;
+use Closure;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Mockery;
+use Tests\DataProviders\GraphQL\RootDataProvider;
+use Tests\DataProviders\TenantDataProvider;
+use Tests\GraphQL\GraphQLSuccess;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\GraphQL\Mutations\DeleteApplicationSettings
+ */
+class DeleteApplicationSettingsTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::__invoke
+     *
+     * @dataProvider dataProviderInvoke
+     */
+    public function testInvoke(
+        Response $expected,
+        Closure $tenantFactory,
+        Closure $userFactory = null,
+    ): void {
+        // Prepare
+        $this->setUser($userFactory, $this->setTenant($tenantFactory));
+
+        // Mock
+        if ($expected instanceof GraphQLSuccess) {
+            $storage = Mockery::mock(Storage::class);
+            $storage
+                ->shouldReceive('delete')
+                ->with(true)
+                ->once()
+                ->andReturn(true);
+
+            $this->app->bind(Storage::class, static function () use ($storage): Storage {
+                return $storage;
+            });
+        }
+
+        // Test
+        $this
+            ->graphQL(/** @lang GraphQL */ '
+                mutation deleteApplicationSettings {
+                    deleteApplicationSettings {
+                        result
+                    }
+                }')
+            ->assertThat($expected);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderInvoke(): array {
+        return (new CompositeDataProvider(
+            new TenantDataProvider(),
+            new RootDataProvider('deleteApplicationSettings'),
+            new ArrayDataProvider([
+                'ok' => [
+                    new GraphQLSuccess('deleteApplicationSettings', DeleteApplicationSettings::class, [
+                        'result' => true,
+                    ]),
+                ],
+            ]),
+        ))->getData();
+    }
+    // </editor-fold>
+}
