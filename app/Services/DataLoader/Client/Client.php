f6b532eb8fe0f15aeff6e9c47c6390309f7e9ad5
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-14 17:32:31 +0400

    Fixed data loader setting prefix (#94)

--- app/Services/DataLoader/Client/Client.php
+++ app/Services/DataLoader/Client/Client.php
@@ -229,7 +229,7 @@ class Client {
     // =========================================================================
     public function isEnabled(): bool {
         return $this->config->get('ep.data_loader.enabled')
-            && $this->config->get('easyportal.data_loader.endpoint');
+            && $this->config->get('ep.data_loader.endpoint');
     }
 
     /**
@@ -242,7 +242,7 @@ class Client {
      */
     public function iterator(string $selector, string $graphql, array $params, Closure $retriever): QueryIterator {
         return (new QueryIterator($this->logger, $this, "data.{$selector}", $graphql, $params, $retriever))
-            ->chunk($this->config->get('easyportal.data_loader.chunk'));
+            ->chunk($this->config->get('ep.data_loader.chunk'));
     }
 
     /**
@@ -269,7 +269,7 @@ class Client {
         }
 
         // Call
-        $url      = $this->config->get('easyportal.data_loader.endpoint');
+        $url      = $this->config->get('ep.data_loader.endpoint');
         $data     = [
             'query'     => $graphql,
             'variables' => $params,
