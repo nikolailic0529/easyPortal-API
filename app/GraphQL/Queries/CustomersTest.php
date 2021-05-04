8b8058ddd4ce322c164640b53c83f4ac4c53d347
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-05-04 17:46:29 +0400

    (query) `CustomersTest` fix.

--- app/GraphQL/Queries/CustomersTest.php
+++ app/GraphQL/Queries/CustomersTest.php
@@ -4,11 +4,16 @@ namespace App\GraphQL\Queries;
 
 use App\Models\Customer;
 use App\Models\Location;
+use App\Models\Organization;
+use App\Models\Reseller;
 use Closure;
 use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\MergeDataProvider;
+use Tests\DataProviders\GraphQL\Tenants\RootTenantDataProvider;
 use Tests\DataProviders\GraphQL\Tenants\TenantDataProvider;
+use Tests\DataProviders\GraphQL\Users\TenantUserDataProvider;
 use Tests\DataProviders\GraphQL\Users\UserDataProvider;
 use Tests\GraphQL\GraphQLPaginated;
 use Tests\TestCase;
@@ -31,11 +36,13 @@ class CustomersTest extends TestCase {
         Closure $customerFactory = null,
     ): void {
         // Prepare
-        $this->setUser($userFactory, $this->setTenant($tenantFactory));
+        $tenant = $this->setTenant($tenantFactory);
+        $user   = $this->setUser($userFactory, $tenant);
+
         $this->setSettings($settings);
 
         if ($customerFactory) {
-            $customerFactory($this);
+            $customerFactory($this, $tenant, $user);
         }
 
         // Test
@@ -93,19 +100,51 @@ class CustomersTest extends TestCase {
      * @return array<mixed>
      */
     public function dataProviderQuery(): array {
-        return (new CompositeDataProvider(
-            new TenantDataProvider('customers'),
-            new UserDataProvider('customers'),
-            new ArrayDataProvider([
-                'ok' => [
-                    new GraphQLPaginated('customers', self::class, [
-                        [
-                            'id'              => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name'            => 'name aaa',
-                            'assets_count'    => 0,
-                            'locations_count' => 1,
-                            'locations'       => [
-                                [
+        return (new MergeDataProvider([
+            'root'   => new CompositeDataProvider(
+                new RootTenantDataProvider('customers'),
+                new TenantUserDataProvider('customers'),
+                new ArrayDataProvider([
+                    'ok' => [
+                        new GraphQLPaginated('customers', null),
+                        [],
+                        static function (TestCase $test, Organization $organization): Customer {
+                            return Customer::factory()->create();
+                        },
+                    ],
+                ]),
+            ),
+            'tenant' => new CompositeDataProvider(
+                new TenantDataProvider('customers'),
+                new UserDataProvider('customers'),
+                new ArrayDataProvider([
+                    'ok' => [
+                        new GraphQLPaginated('customers', self::class, [
+                            [
+                                'id'              => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                                'name'            => 'name aaa',
+                                'assets_count'    => 0,
+                                'locations_count' => 1,
+                                'locations'       => [
+                                    [
+                                        'id'        => 'f9396bc1-2f2f-4c58-2f2f-7a224ac20944',
+                                        'state'     => 'state1',
+                                        'postcode'  => '19911',
+                                        'line_one'  => 'line_one_data',
+                                        'line_two'  => 'line_two_data',
+                                        'latitude'  => '47.91634204',
+                                        'longitude' => '-2.26318359',
+                                    ],
+                                ],
+                                'contacts_count'  => 1,
+                                'contacts'        => [
+                                    [
+                                        'name'        => 'contact1',
+                                        'email'       => 'contact1@test.com',
+                                        'phone_valid' => false,
+                                    ],
+                                ],
+                                'headquarter'     => [
                                     'id'        => 'f9396bc1-2f2f-4c58-2f2f-7a224ac20944',
                                     'state'     => 'state1',
                                     'postcode'  => '19911',
@@ -115,63 +154,51 @@ class CustomersTest extends TestCase {
                                     'longitude' => '-2.26318359',
                                 ],
                             ],
-                            'contacts_count'  => 1,
-                            'contacts'        => [
-                                [
+                        ]),
+                        [
+                            'ep.headquarter_type' => 'f9834bc1-2f2f-4c57-bb8d-7a224ac24985',
+                        ],
+                        static function (TestCase $test, Organization $organization): void {
+                            // static function will throw error
+                            $customer = Customer::factory()
+                                ->hasContacts(1, [
                                     'name'        => 'contact1',
                                     'email'       => 'contact1@test.com',
                                     'phone_valid' => false,
-                                ],
-                            ],
-                            'headquarter'     => [
-                                'id'        => 'f9396bc1-2f2f-4c58-2f2f-7a224ac20944',
-                                'state'     => 'state1',
-                                'postcode'  => '19911',
-                                'line_one'  => 'line_one_data',
-                                'line_two'  => 'line_two_data',
-                                'latitude'  => '47.91634204',
-                                'longitude' => '-2.26318359',
-                            ],
-                        ],
-                    ]),
-                    [
-                        'ep.headquarter_type' => 'f9834bc1-2f2f-4c57-bb8d-7a224ac24985',
+                                ])
+                                ->create([
+                                    'id'              => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                                    'name'            => 'name aaa',
+                                    'assets_count'    => 0,
+                                    'contacts_count'  => 1,
+                                    'locations_count' => 1,
+                                ]);
+
+                            $customer->resellers()->attach(Reseller::factory()->create([
+                                'id' => $organization,
+                            ]));
+
+                            Location::factory()
+                                ->hasTypes(1, [
+                                    'id'   => 'f9834bc1-2f2f-4c57-bb8d-7a224ac24985',
+                                    'name' => 'headquarter',
+                                ])
+                                ->create([
+                                    'id'          => 'f9396bc1-2f2f-4c58-2f2f-7a224ac20944',
+                                    'state'       => 'state1',
+                                    'postcode'    => '19911',
+                                    'line_one'    => 'line_one_data',
+                                    'line_two'    => 'line_two_data',
+                                    'latitude'    => '47.91634204',
+                                    'longitude'   => '-2.26318359',
+                                    'object_type' => $customer->getMorphClass(),
+                                    'object_id'   => $customer->getKey(),
+                                ]);
+                        },
                     ],
-                    static function (): void {
-                        // static function will throw error
-                        $customer = Customer::factory()
-                            ->hasContacts(1, [
-                                'name'        => 'contact1',
-                                'email'       => 'contact1@test.com',
-                                'phone_valid' => false,
-                            ])
-                            ->create([
-                                'id'              => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                                'name'            => 'name aaa',
-                                'assets_count'    => 0,
-                                'contacts_count'  => 1,
-                                'locations_count' => 1,
-                            ]);
-                        Location::factory()
-                            ->hasTypes(1, [
-                                'id'   => 'f9834bc1-2f2f-4c57-bb8d-7a224ac24985',
-                                'name' => 'headquarter',
-                            ])
-                            ->create([
-                                'id'          => 'f9396bc1-2f2f-4c58-2f2f-7a224ac20944',
-                                'state'       => 'state1',
-                                'postcode'    => '19911',
-                                'line_one'    => 'line_one_data',
-                                'line_two'    => 'line_two_data',
-                                'latitude'    => '47.91634204',
-                                'longitude'   => '-2.26318359',
-                                'object_type' => $customer->getMorphClass(),
-                                'object_id'   => $customer->getKey(),
-                            ]);
-                    },
-                ],
-            ]),
-        ))->getData();
+                ]),
+            ),
+        ]))->getData();
     }
     // </editor-fold>
 }
