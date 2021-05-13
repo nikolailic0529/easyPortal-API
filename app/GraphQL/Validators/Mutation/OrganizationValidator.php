98304270f7526ce526750d975c488d3a8387f39d
Mohamed <mohammedosama.amagd@gmail.com>
2021-05-13 09:06:12 +0200

    Remove `exsits` validation rule (#164)
    Closes:
    - https://thefas.atlassian.net/browse/EAP-180

--- app/GraphQL/Validators/Mutation/OrganizationValidator.php
+++ app/GraphQL/Validators/Mutation/OrganizationValidator.php
@@ -2,7 +2,7 @@
 
 namespace App\GraphQL\Validators\Mutation;
 
-use App\Models\Currency;
+use App\Rules\CurrencyId;
 use App\Rules\Locale;
 use Illuminate\Contracts\Config\Repository;
 use Nuwave\Lighthouse\Validation\Validator;
@@ -19,14 +19,13 @@ class OrganizationValidator extends Validator {
      * @return array<string, array<mixed>>
      */
     public function rules(): array {
-        $currenciesTable = (new Currency())->getTable();
-        $maxSize         = $this->config->get('ep.image.max_size');
-        $formats         = $this->config->get('ep.image.formats');
-        $mimeTypes       = implode(',', $formats);
+        $maxSize   = $this->config->get('ep.image.max_size');
+        $formats   = $this->config->get('ep.image.formats');
+        $mimeTypes = implode(',', $formats);
 
         return [
             'locale'                   => [new Locale() ],
-            'currency_id'              => ["exists:{$currenciesTable},id"],
+            'currency_id'              => ['nullable', new CurrencyId()],
             'branding_dark_theme'      => ['nullable', 'boolean'],
             'branding_primary_color'   => ['nullable', 'regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/'],
             'branding_secondary_color' => ['nullable', 'regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/'],
