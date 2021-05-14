070fa18a5da6df8b7edcba9efb2f7347a340109e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-05-14 16:31:56 +0400

    Added `root` argument to `@organization` directive (if `true` only root organization allowed).

--- app/GraphQL/Directives/Directives/Organization.php
+++ app/GraphQL/Directives/Directives/Organization.php
@@ -20,13 +20,44 @@ abstract class Organization extends AuthDirective implements FieldMiddleware {
             """
             Authenticated user must be a member of the current organization.
             """
-            directive @organization on FIELD_DEFINITION | OBJECT
+            directive @organization(
+              """
+              Authenticated user must be a member of the root organization.
+              """
+              root: Boolean! = false
+            ) on FIELD_DEFINITION | OBJECT
             GRAPHQL;
     }
 
     protected function isAuthorized(Authenticatable|null $user): bool {
         return $user instanceof HasOrganization
             && $this->organization->defined()
-            && $this->organization->is($user->getOrganization());
+            && $this->organization->is($user->getOrganization())
+            && (!$this->isRootRequired() || $this->organization->isRoot());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getRequirements(): array {
+        $root         = $this->isRootRequired();
+        $requirements = [];
+
+        if ($root) {
+            $definition = $this->getDefinitionNode();
+            $argument   = $this->getArgDefinitionNode($definition, 'root');
+
+            $requirements = [
+                "<{$this->name()}(root)>" => $argument->description?->value,
+            ];
+        } else {
+            $requirements = parent::getRequirements();
+        }
+
+        return $requirements;
+    }
+
+    protected function isRootRequired(): bool {
+        return (bool) $this->directiveArgValue('root');
     }
 }
