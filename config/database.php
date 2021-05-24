d62250b08de8b018b2e0e782667f1d91b28f7e59
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-05-24 13:34:23 +0400

    Default collation for mysql set to 'utf8mb4_0900_as_ci'.

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'password'       => env('DB_PASSWORD', ''),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => 'utf8mb4',
-            'collation'      => 'utf8mb4_0900_ai_ci',
+            'collation'      => 'utf8mb4_0900_as_ci',
             'timezone'       => '+00:00',
             'prefix'         => '',
             'prefix_indexes' => true,
