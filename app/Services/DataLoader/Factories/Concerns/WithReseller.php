f1cf01dccd33d07c8a740ec6505b6f4b6eb2a0da
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-06-08 10:33:08 +0400

    `WithReseller::reseller()` will support `AssetDocument` and `Document`.

--- app/Services/DataLoader/Factories/Concerns/WithReseller.php
+++ app/Services/DataLoader/Factories/Concerns/WithReseller.php
@@ -6,22 +6,35 @@ use App\Models\Reseller;
 use App\Services\DataLoader\Exceptions\ResellerNotFoundException;
 use App\Services\DataLoader\Resolvers\ResellerResolver;
 use App\Services\DataLoader\Schema\Asset;
+use App\Services\DataLoader\Schema\AssetDocument;
+use App\Services\DataLoader\Schema\Document;
 
 trait WithReseller {
     abstract protected function getResellerResolver(): ResellerResolver;
 
-    protected function reseller(Asset $asset): ?Reseller {
-        $id       = $asset->resellerId ?? null;
+    protected function reseller(Asset|Document|AssetDocument $object): ?Reseller {
+        // Id
+        $id = null;
+
+        if ($object instanceof AssetDocument) {
+            $id = $object->reseller->id ?? null;
+        } else {
+            $id = $object->resellerId ?? null;
+        }
+
+        // Search
         $reseller = null;
 
         if ($id) {
             $reseller = $this->getResellerResolver()->get($id);
         }
 
+        // Found?
         if ($id && !$reseller) {
-            throw new ResellerNotFoundException($id, $asset);
+            throw new ResellerNotFoundException($id, $object);
         }
 
+        // Return
         return $reseller;
     }
 }
--- app/Services/DataLoader/Factories/Concerns/WithResellerTest.php
+++ app/Services/DataLoader/Factories/Concerns/WithResellerTest.php
@@ -6,6 +6,9 @@ use App\Models\Reseller;
 use App\Services\DataLoader\Exceptions\ResellerNotFoundException;
 use App\Services\DataLoader\Resolvers\ResellerResolver;
 use App\Services\DataLoader\Schema\Asset;
+use App\Services\DataLoader\Schema\AssetDocument;
+use App\Services\DataLoader\Schema\Document;
+use Closure;
 use Mockery;
 use Tests\TestCase;
 
@@ -14,10 +17,14 @@ use Tests\TestCase;
  * @coversDefaultClass \App\Services\DataLoader\Factories\Concerns\WithReseller
  */
 class WithResellerTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
     /**
      * @covers ::reseller
+     *
+     * @dataProvider dataProviderReseller
      */
-    public function testResellerExistsThroughProvider(): void {
+    public function testResellerExistsThroughProvider(Closure $objectFactory): void {
         $reseller = Reseller::factory()->make();
         $resolver = Mockery::mock(ResellerResolver::class);
         $resolver
@@ -27,38 +34,18 @@ class WithResellerTest extends TestCase {
             ->andReturn($reseller);
 
         $factory = new WithResellerTestObject($resolver);
+        $object  = $objectFactory($this, $reseller);
 
-        $this->assertEquals($reseller, $factory->reseller(new Asset([
-            'id'         => $this->faker->uuid,
-            'resellerId' => $reseller->getKey(),
-        ])));
+        $this->assertEquals($reseller, $factory->reseller($object));
     }
 
     /**
      * @covers ::reseller
+     *
+     * @dataProvider dataProviderReseller
      */
-    public function testResellerAssetWithoutReseller(): void {
-        $resolver = Mockery::mock(ResellerResolver::class);
-        $resolver
-            ->shouldReceive('get')
-            ->never();
-
-        $factory = new WithResellerTestObject($resolver);
-
-        $this->assertNull($factory->reseller(new Asset([
-            'id' => $this->faker->uuid,
-        ])));
-    }
-
-    /**
-     * @covers ::reseller
-     */
-    public function testResellerResellerNotFound(): void {
+    public function testResellerResellerNotFound(Closure $objectFactory): void {
         $reseller = Reseller::factory()->make();
-        $asset    = new Asset([
-            'id'         => $this->faker->uuid,
-            'resellerId' => $reseller->getKey(),
-        ]);
         $resolver = Mockery::mock(ResellerResolver::class);
         $resolver
             ->shouldReceive('get')
@@ -67,11 +54,62 @@ class WithResellerTest extends TestCase {
             ->andReturn(null);
 
         $factory = new WithResellerTestObject($resolver);
+        $object  = $objectFactory($this, $reseller);
 
         $this->expectException(ResellerNotFoundException::class);
 
-        $this->assertEquals($reseller, $factory->reseller($asset));
+        $this->assertEquals($reseller, $factory->reseller($object));
     }
+
+    /**
+     * @covers ::reseller
+     */
+    public function testResellerAssetWithoutReseller(): void {
+        $object   = new Asset();
+        $resolver = Mockery::mock(ResellerResolver::class);
+        $resolver
+            ->shouldReceive('get')
+            ->never();
+
+        $factory = new WithResellerTestObject($resolver);
+
+        $this->assertNull($factory->reseller($object));
+    }
+    //</editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderReseller(): array {
+        return [
+            AssetDocument::class => [
+                static function (TestCase $test, Reseller $reseller) {
+                    return new AssetDocument([
+                        'reseller' => [
+                            'id' => $reseller->getKey(),
+                        ],
+                    ]);
+                },
+            ],
+            Document::class      => [
+                static function (TestCase $test, Reseller $reseller) {
+                    return new Document([
+                        'resellerId' => $reseller->getKey(),
+                    ]);
+                },
+            ],
+            Asset::class         => [
+                static function (TestCase $test, Reseller $reseller) {
+                    return new Asset([
+                        'resellerId' => $reseller->getKey(),
+                    ]);
+                },
+            ],
+        ];
+    }
+    // </editor-fold>
 }
 
 // @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
