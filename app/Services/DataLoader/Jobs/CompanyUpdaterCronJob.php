6a8ace56b2a1408b802a9c8cd11278a28c22eb3b
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-06-11 13:38:55 +0400

    Added `CompanyUpdaterCronJob` that implements base to import/update companies.

---
+++ app/Services/DataLoader/Jobs/CompanyUpdaterCronJob.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Jobs;
+
+use App\Jobs\NamedJob;
+use App\Models\Model;
+use App\Services\DataLoader\Client\Client;
+use App\Services\DataLoader\Client\QueryIterator;
+use App\Services\DataLoader\DataLoaderService;
+use App\Services\DataLoader\Factory;
+use App\Services\DataLoader\FactoryPrefetchable;
+use App\Services\DataLoader\Schema\Company;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use LastDragon_ru\LaraASP\Queue\Configs\QueueableConfig;
+use LastDragon_ru\LaraASP\Queue\QueueableConfigurator;
+use LastDragon_ru\LaraASP\Queue\Queueables\CronJob;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+use function sprintf;
+
+abstract class CompanyUpdaterCronJob extends CronJob implements ShouldBeUnique, NamedJob {
+    public function handle(
+        Container $container,
+        LoggerInterface $logger,
+        DataLoaderService $service,
+        QueueableConfigurator $configurator,
+    ): void {
+        $client    = $service->getClient();
+        $config    = $configurator->config($this);
+        $factory   = $this->getFactory($container);
+        $companies = $this->getCompanies($client, $config)->each(
+            static function (array $companies) use ($factory): void {
+                if ($factory instanceof FactoryPrefetchable) {
+                    $factory->prefetch($companies, true);
+                }
+            },
+        );
+
+        foreach ($companies as $company) {
+            try {
+                $model = $factory->find($company);
+
+                if ($model) {
+                    $this->updateExistingCompany($container, $company, $model);
+                } else {
+                    $this->updateCreatedCompany($container, $company, $factory->create($company));
+                }
+            } catch (Throwable $exception) {
+                $logger->warning(sprintf('%s failed.', $this::class), [
+                    'company'   => $company,
+                    'exception' => $exception,
+                ]);
+            }
+        }
+    }
+
+    abstract protected function getFactory(Container $container): Factory;
+
+    /**
+     * @return \App\Services\DataLoader\Client\QueryIterator<\App\Services\DataLoader\Schema\Company>
+     */
+    abstract protected function getCompanies(Client $client, QueueableConfig $config): QueryIterator;
+
+    abstract protected function updateCreatedCompany(Container $container, Company $company, Model $model): void;
+
+    abstract protected function updateExistingCompany(Container $container, Company $company, Model $model): void;
+}
