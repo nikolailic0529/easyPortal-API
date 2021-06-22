1fcb751ba36a067933890ae44e5bd4fe3e562e42
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-06-22 11:58:14 +0400

    Added base `Import` command, `ImportAssets` will extends it.

--- app/Services/DataLoader/Commands/ImportAssets.php
+++ app/Services/DataLoader/Commands/ImportAssets.php
@@ -2,149 +2,21 @@
 
 namespace App\Services\DataLoader\Commands;
 
-use App\Models\Concerns\GlobalScopes\GlobalScopes;
-use App\Services\DataLoader\Commands\Concerns\WithBooleanOptions;
 use App\Services\DataLoader\Importers\AssetsImporter;
-use App\Services\DataLoader\Importers\Status;
-use App\Services\DataLoader\Schema\TypeWithId;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Date;
 
-use function end;
-use function filter_var;
-use function reset;
-use function str_pad;
-
-use const FILTER_VALIDATE_INT;
-use const STR_PAD_LEFT;
-
-class ImportAssets extends Command {
-    use GlobalScopes;
-    use WithBooleanOptions;
-
+class ImportAssets extends Import {
     /**
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
-     *
-     * @var string
+     * @inheritDoc
      */
-    protected $signature = 'ep:data-loader-import-assets
-        {--u|update : Update asset if exists}
-        {--U|no-update : Do not update asset if exists (default)}
-        {--continue= : continue processing from given asset}
-        {--from= : start processing from given datetime}
-        {--limit= : max assets to process}
-        {--chunk= : chunk size}
-    ';
+    protected function getReplacements(): array {
+        return [
+            '${command}' => 'ep:data-loader-import-assets',
+            '${objects}' => 'assets',
+            '${object}'  => 'asset',
+        ];
+    }
 
-    /**
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
-     *
-     * @var string
-     */
-    protected $description = 'Import all assets.';
-
-    public function handle(
-        AssetsImporter $importer,
-    ): int {
-        // Settings
-        $from     = $this->option('from');
-        $chunk    = $this->option('chunk');
-        $limit    = $this->option('limit');
-        $update   = $this->getBooleanOption('update', false);
-        $continue = $this->option('continue');
-
-        $this->line('Settings:');
-
-        if ($from) {
-            $from = Date::make($from);
-
-            $this->line("    From:      {$from->toISOString()}");
-        }
-
-        if ($chunk) {
-            $chunk = (int) $chunk;
-            $this->line("    Chunk:     {$chunk}");
-        }
-
-        if ($limit) {
-            $limit = (int) $limit;
-            $this->line("    Limit:     {$limit}");
-        }
-
-        if ($continue) {
-            if (filter_var($continue, FILTER_VALIDATE_INT)) {
-                $continue = (int) $continue;
-            }
-
-            $this->line("    Continue:  {$continue}");
-        }
-
-        if ($chunk || $limit || $continue) {
-            $this->newLine();
-        }
-
-        // Begin
-        $this->line(
-            'Chunk #   : From                                 ... To                                   -  Processed',
-        );
-        $this->line(
-            '            C:    created;        U:    updated;     F:     failed;',
-        );
-
-        // Process
-        $length   = 10;
-        $previous = new Status();
-
-        $importer
-            ->onChange(function (array $items, Status $status) use (&$previous, $length): void {
-                $chunk        = str_pad((string) $status->chunk, $length, '0', STR_PAD_LEFT);
-                $chunkFailed  = str_pad((string) ($status->failed - $previous->failed), $length, ' ', STR_PAD_LEFT);
-                $chunkCreated = str_pad((string) ($status->created - $previous->created), $length, ' ', STR_PAD_LEFT);
-                $chunkUpdated = str_pad((string) ($status->updated - $previous->updated), $length, ' ', STR_PAD_LEFT);
-                $processed    = str_pad((string) $status->processed, $length, ' ', STR_PAD_LEFT);
-                $first        = reset($items);
-                $first        = str_pad($first instanceof TypeWithId ? $first->id : 'null', 36, ' ', STR_PAD_LEFT);
-                $last         = end($items);
-                $last         = str_pad($last instanceof TypeWithId ? $last->id : 'null', 36, ' ', STR_PAD_LEFT);
-                $lineOne      = "{$chunk}: {$first} ... {$last} - {$processed}";
-                $lineTwo      = "            C: {$chunkCreated};        U: {$chunkUpdated};     F: {$chunkFailed};";
-
-                if ($status->failed - $previous->failed > 0) {
-                    $this->warn($lineOne);
-                    $this->warn($lineTwo);
-                } else {
-                    $this->info($lineOne);
-                    $this->info($lineTwo);
-                }
-
-                $previous = clone $status;
-            })
-            ->onFinish(function (Status $status) use ($length): void {
-                $this->newLine();
-
-                $processed = str_pad((string) $status->processed, $length, ' ', STR_PAD_LEFT);
-                $created   = str_pad((string) $status->created, $length, ' ', STR_PAD_LEFT);
-                $updated   = str_pad((string) $status->updated, $length, ' ', STR_PAD_LEFT);
-                $failed    = str_pad((string) $status->failed, $length, ' ', STR_PAD_LEFT);
-
-                $this->line("Processed: {$processed}");
-                $this->line("Created:   {$created}");
-                $this->line("Updated:   {$updated}");
-
-                if ($status->failed) {
-                    $this->warn("Failed:    {$failed}");
-                } else {
-                    $this->line("Failed:    {$failed}");
-                }
-
-                $this->newLine();
-            })
-            ->import($update, $from, $continue, $chunk, $limit);
-
-        // Done
-        $this->info('Done.');
-
-        // Return
-        return self::SUCCESS;
+    public function handle(AssetsImporter $importer): int {
+        return $this->process($importer);
     }
 }
