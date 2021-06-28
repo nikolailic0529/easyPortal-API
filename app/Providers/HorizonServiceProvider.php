aad7192eb6eea51dc5b1b0d6a43cc5fe4dcc9b24
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-06-28 09:44:54 +0400

    Horizon installed.

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider {
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+
+        // Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void {
+        Gate::define('viewHorizon', static function (Authenticatable|null $user): bool {
+            return false;
+        });
+    }
+}
