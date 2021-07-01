90025fe20b7b15aba71bee9d69bd4d77c1301710
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-01 17:00:01 +0400

    Implemented `Application.logs` query.

---
+++ app/GraphQL/Queries/Application/Logs.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Queries\Application;
+
+use App\Services\Logger\Models\Log;
+use Illuminate\Database\Eloquent\Builder;
+
+class Logs {
+    public function __construct() {
+        // empty
+    }
+
+    public function __invoke(Builder $builder): Builder {
+        return Log::query()->whereNull('parent_id');
+    }
+}
