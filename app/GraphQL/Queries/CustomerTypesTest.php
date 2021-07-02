2c2e0281e6b987fa0ec0e8249214e64bf6fbea99
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-02 16:39:31 +0400

    `customerTypes` updated.

--- app/GraphQL/Queries/CustomerTypesTest.php
+++ app/GraphQL/Queries/CustomerTypesTest.php
@@ -9,8 +9,8 @@ use Illuminate\Translation\Translator;
 use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
-use Tests\DataProviders\GraphQL\Organizations\AnyOrganizationDataProvider;
-use Tests\DataProviders\GraphQL\Users\AnyUserDataProvider;
+use Tests\DataProviders\GraphQL\Organizations\OrganizationDataProvider;
+use Tests\DataProviders\GraphQL\Users\AuthUserDataProvider;
 use Tests\GraphQL\GraphQLSuccess;
 use Tests\TestCase;
 
@@ -60,11 +60,15 @@ class CustomerTypesTest extends TestCase {
      */
     public function dataProviderInvoke(): array {
         return (new CompositeDataProvider(
-            new AnyOrganizationDataProvider('customerTypes'),
-            new AnyUserDataProvider(),
+            new OrganizationDataProvider('customerTypes'),
+            new AuthUserDataProvider('customerTypes'),
             new ArrayDataProvider([
                 'ok' => [
                     new GraphQLSuccess('customerTypes', CustomerTypes::class, [
+                        [
+                            'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                            'name' => 'No translation',
+                        ],
                         [
                             'id'   => '6f19ef5f-5963-437e-a798-29296db08d59',
                             'name' => 'Translated (locale)',
@@ -72,10 +76,6 @@ class CustomerTypesTest extends TestCase {
                         [
                             'id'   => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
                             'name' => 'Translated (fallback)',
-                        ],
-                        [
-                            'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name' => 'No translation',
                         ],
                     ]),
                     static function (TestCase $test): string {
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -3,35 +3,31 @@
 namespace App\Models;
 
 use App\GraphQL\Contracts\Translatable;
+use App\Models\Concerns\HasCustomers;
 use App\Models\Concerns\TranslateProperties;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Type.
  *
- * @property string                       $id
- * @property string                       $object_type
- * @property string                       $key
- * @property string                       $name
- * @property \Carbon\CarbonImmutable      $created_at
- * @property \Carbon\CarbonImmutable      $updated_at
- * @property \Carbon\CarbonImmutable|null $deleted_at
+ * @property string                                                              $id
+ * @property string                                                              $object_type
+ * @property string                                                              $key
+ * @property string                                                              $name
+ * @property \Carbon\CarbonImmutable                                             $created_at
+ * @property \Carbon\CarbonImmutable                                             $updated_at
+ * @property \Carbon\CarbonImmutable|null                                        $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<\App\Models\Customer> $customers
  * @method static \Database\Factories\TypeFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereObjectType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereKey($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Type whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Type extends PolymorphicModel implements Translatable {
     use HasFactory;
     use TranslateProperties;
+    use HasCustomers;
 
     /**
      * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
