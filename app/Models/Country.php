822f8c7474c39e49ec31b29e500aaeeab7f089ed
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-02 15:23:17 +0400

    `countries` updated.

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,33 +3,36 @@
 namespace App\Models;
 
 use App\GraphQL\Contracts\Translatable;
+use App\Models\Concerns\HasAssetsThroughLocations;
+use App\Models\Concerns\HasCities;
+use App\Models\Concerns\HasCustomersThroughLocations;
 use App\Models\Concerns\TranslateProperties;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Country.
  *
- * @property string                       $id
- * @property string                       $code
- * @property string                       $name
- * @property \Carbon\CarbonImmutable      $created_at
- * @property \Carbon\CarbonImmutable      $updated_at
- * @property \Carbon\CarbonImmutable|null $deleted_at
+ * @property string                                                              $id
+ * @property string                                                              $code
+ * @property string                                                              $name
+ * @property \Carbon\CarbonImmutable                                             $created_at
+ * @property \Carbon\CarbonImmutable                                             $updated_at
+ * @property \Carbon\CarbonImmutable|null                                        $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<\App\Models\Asset>    $assets
+ * @property-read \Illuminate\Database\Eloquent\Collection<\App\Models\City>     $cities
+ * @property-read \Illuminate\Database\Eloquent\Collection<\App\Models\Customer> $customers
  * @method static \Database\Factories\CountryFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereCode($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Country whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Country extends Model implements Translatable {
     use HasFactory;
     use TranslateProperties;
+    use HasCities;
+    use HasAssetsThroughLocations;
+    use HasCustomersThroughLocations;
 
     /**
      * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
