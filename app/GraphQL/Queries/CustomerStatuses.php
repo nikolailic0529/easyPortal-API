e89198149e809d3201ed5b091090416a0197f133
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-02 16:33:16 +0400

    `customerStatuses` updated.

--- app/GraphQL/Queries/CustomerStatuses.php
+++ app/GraphQL/Queries/CustomerStatuses.php
@@ -3,17 +3,10 @@
 namespace App\GraphQL\Queries;
 
 use App\Models\Customer;
-use App\Models\Status;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 class CustomerStatuses {
-    /**
-     * @param array<string, mixed> $args
-     */
-    public function __invoke(mixed $_, array $args): Collection {
-        return Status::query()
-            ->where('object_type', '=', (new Customer())->getMorphClass())
-            ->orderByKey()
-            ->get();
+    public function __invoke(Builder $builder): Builder {
+        return $builder->where('object_type', '=', (new Customer())->getMorphClass());
     }
 }
