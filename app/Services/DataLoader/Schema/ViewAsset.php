bb538f6bc7dcde69f190e7dedea42486f550602a
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-06 16:22:54 +0400

    "Cannot assign null to property App\\Services\\DataLoader\\Schema\\ViewAsset::$assetCoverage of type string" fix
    Issue: https://thefas.atlassian.net/browse/EAP-360

--- app/Services/DataLoader/Schema/ViewAsset.php
+++ app/Services/DataLoader/Schema/ViewAsset.php
@@ -23,7 +23,7 @@ class ViewAsset extends Type implements TypeWithId {
     public ?string  $longitude;
     public ?string  $countryCode;
     public string   $status;
-    public string   $assetCoverage;
+    public ?string  $assetCoverage;
     public string   $dataQualityScore;
 
     /**
