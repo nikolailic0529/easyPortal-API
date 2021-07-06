6931ee01f296c278f9d4852dfa2f50d48bf94145
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-06 12:05:30 +0400

    Implemented `contractsAggregate` query.

--- app/GraphQL/Resolvers/AggregateResolver.php
+++ app/GraphQL/Resolvers/AggregateResolver.php
@@ -3,19 +3,25 @@
 namespace App\GraphQL\Resolvers;
 
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as DatabaseBuilder;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
-use stdClass;
 
 abstract class AggregateResolver {
     /**
      * @param array<string,mixed> $args
      */
-    public function __invoke(mixed $root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): stdClass {
-        $result = $resolveInfo->argumentSet->enhanceBuilder($this->getQuery(), [])->first();
+    public function __invoke(mixed $root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): mixed {
+        $query  = $this->getQuery();
+        $result = $resolveInfo->argumentSet->enhanceBuilder($query, []);
+        $result = $this->getResult($result);
 
         return $result;
     }
 
-    abstract protected function getQuery(): Builder;
+    protected function getResult(DatabaseBuilder|EloquentBuilder $builder): mixed {
+        return $builder->first();
+    }
+
+    abstract protected function getQuery(): DatabaseBuilder|EloquentBuilder;
 }
