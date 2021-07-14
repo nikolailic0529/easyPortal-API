0993c4f99cb52a5c30329d501ccfb8735a8f168d
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-07-15 00:35:14 +0200

    path => url

--- app/GraphQL/Queries/QuoteTest.php
+++ app/GraphQL/Queries/QuoteTest.php
@@ -795,7 +795,7 @@ class QuoteTest extends TestCase {
                                             [
                                                 'id'   => 'f9834bc1-2f2f-4c57-bb8d-7a224ac2E988',
                                                 'name' => 'document',
-                                                'path' => 'http://example.com/document.csv',
+                                                'url'  => 'http://example.com/document.csv',
                                             ],
                                         ],
                                     ],
