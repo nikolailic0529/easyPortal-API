574aea964dc431e58399bad720cddb3433832f05
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-07-16 16:45:13 +0200

    fix missed class usage

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\CascadeDeletes\CascadeDeletable;
 use App\Models\Concerns\HasDocument;
 use App\Models\Concerns\SyncHasMany;
 use App\Services\Organization\Eloquent\OwnedByOrganization;
