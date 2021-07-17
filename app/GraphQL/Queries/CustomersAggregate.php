3d613c98826dff5cdf4c5d4b03cdc331677a4584
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-07-17 10:08:16 +0400

    `customersAggregate()` will work with relations. (#358)

--- app/GraphQL/Queries/CustomersAggregate.php
+++ app/GraphQL/Queries/CustomersAggregate.php
@@ -4,15 +4,15 @@ namespace App\GraphQL\Queries;
 
 use App\GraphQL\Resolvers\AggregateResolver;
 use App\Models\Customer;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as DatabaseBuilder;
 
 class CustomersAggregate extends AggregateResolver {
-    protected function getQuery(): Builder {
+    protected function getQuery(): DatabaseBuilder|EloquentBuilder {
         $model = new Customer();
         $query = $model->query()
             ->selectRaw("COUNT(DISTINCT {$model->qualifyColumn($model->getKeyName())}) as count")
-            ->selectRaw("SUM({$model->qualifyColumn('assets_count')}) as assets")
-            ->toBase();
+            ->selectRaw("SUM({$model->qualifyColumn('assets_count')}) as assets");
 
         return $query;
     }
