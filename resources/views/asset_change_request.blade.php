a20610d57c2a2c6bd664640ccc7bc9b0ed0c7bcc
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-07-22 11:11:37 +0200

    update email id

--- resources/views/asset_change_request.blade.php
+++ resources/views/asset_change_request.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # {{ config('app.name') }}
 
-A new asset change request from {{ $request->from }} to change asset {{ $request->asset_id }}.
+A new asset change request from {{ $request->from }} to change asset {{ $request->object_id }}.
 
 {{ $request->message }}
 
