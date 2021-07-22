a07f00b2f807b611b8a9ac7f49b9ad9b3c2ed84d
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-07-22 10:48:09 +0200

    update database.mwb

--- database/migrations/2021_07_21_141924_create_change_request_table~up.sql
+++ database/migrations/2021_07_21_141924_create_change_request_table~up.sql
@@ -17,17 +17,18 @@ CREATE TABLE IF NOT EXISTS `change_requests` (
   `created_at`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at`      TIMESTAMP    NULL DEFAULT NULL,
+  `deleted_not`     TINYINT(1)   GENERATED ALWAYS AS (if((`deleted_at` is null), 1, NULL)) STORED,
   PRIMARY KEY (`id`),
-  INDEX `fk_asset_change_requests_organizations1_idx` (`organization_id` ASC) VISIBLE,
-  INDEX `fk_asset_change_requests_users1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_change_requests_organizations1_idx` (`organization_id` ASC) VISIBLE,
+  INDEX `fk_change_requests_users1_idx` (`user_id` ASC) VISIBLE,
   INDEX `idx__deleted_at` (`deleted_at` ASC) VISIBLE,
-  INDEX `idx__object_id__object_type__deleted_at` (`object_id` ASC, `object_type` ASC, `deleted_at` ASC) VISIBLE,
-  CONSTRAINT `fk_asset_change_requests_organizations1`
+  INDEX `idx__object_id__object_type__deleted_not` (`object_id` ASC, `object_type` ASC, `deleted_not` ASC) VISIBLE,
+  CONSTRAINT `fk_change_requests_organizations1`
     FOREIGN KEY (`organization_id`)
     REFERENCES `organizations` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
-  CONSTRAINT `fk_asset_change_requests_users1`
+  CONSTRAINT `fk_change_requests_users1`
     FOREIGN KEY (`user_id`)
     REFERENCES `users` (`id`)
     ON DELETE RESTRICT
