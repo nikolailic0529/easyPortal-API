9ab3a0f4c662fd0e5339c5196a38b5c6bc739ebf
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-02 17:25:11 +0400

    (tests) Added `WithScout` trait (set up Scout+ElasticSearch for tests).

---
+++ tests/WithScout.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace Tests;
+
+use Elasticsearch\Client;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Support\Collection;
+
+use function str_starts_with;
+
+/**
+ * @mixin \Tests\TestCase
+ */
+trait WithScout {
+    protected function setUpWithScout(): void {
+        // Right now only ElasticSearch is supported.
+        $this->setSettings([
+            'scout.driver'                           => 'elastic',
+            'scout.prefix'                           => 'testing_',
+            'scout.queue'                            => false,
+            'elastic.scout_driver.refresh_documents' => true,
+        ]);
+
+        // Remove all indexes
+        $client  = $this->app->make(Client::class);
+        $prefix  = $this->app->make(Repository::class)->get('scout.prefix');
+        $indexes = (new Collection($client->cat()->indices()))
+            ->map(static function (array $index): string {
+                return $index['index'];
+            })
+            ->filter(static function (string $index) use ($prefix): bool {
+                return str_starts_with($index, $prefix);
+            })
+            ->join(',');
+
+        if ($indexes) {
+            $client->indices()->delete(['index' => $indexes]);
+        }
+    }
+
+    protected function tearDownWithScout(): void {
+        // empty
+    }
+}
