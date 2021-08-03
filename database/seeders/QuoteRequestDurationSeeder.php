bc94c90f88e6ffe1ec3da2b8b96a9fc03e0cb77e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-03 11:58:29 +0400

    `DatabaseSeeder` will seed organizations and durations.

--- database/seeders/QuoteRequestDurationSeeder.php
+++ database/seeders/QuoteRequestDurationSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\QuoteRequestDuration;
+use Illuminate\Support\Str;
 use LastDragon_ru\LaraASP\Migrator\Seeders\SmartSeeder;
 
 use function range;
@@ -11,8 +12,8 @@ class QuoteRequestDurationSeeder extends SmartSeeder {
     public function seed(): void {
         foreach (range(1, 20) as $number) {
             $duration       = new QuoteRequestDuration();
-            $value          = "$number Years";
-            $duration->key  = $value;
+            $value          = $number > 1 ? "{$number} Years" : "{$number} Year";
+            $duration->key  = Str::slug($value);
             $duration->name = $value;
             $duration->save();
         }
