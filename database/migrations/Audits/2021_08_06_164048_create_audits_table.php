a12e822fb8c5fd3386a1e270a2a9e0219cc434cf
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-08-06 19:58:52 +0200

    fix migrations

--- database/migrations/Audits/2021_08_06_164048_create_audits_table.php
+++ database/migrations/Audits/2021_08_06_164048_create_audits_table.php
@@ -1,11 +1,9 @@
 <?php declare(strict_types = 1);
 
-// @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
-
 use App\Services\Audit\Auditor;
 use LastDragon_ru\LaraASP\Migrator\Migrations\RawMigration;
 
-class CreateAuditsTables extends RawMigration {
+return new class() extends RawMigration {
     /**
      * The name of the database connection to use.
      *
@@ -16,4 +14,4 @@ class CreateAuditsTables extends RawMigration {
     protected $connection = Auditor::CONNECTION;
 
     // Please see the associated SQL files
-}
+};
