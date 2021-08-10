6278dffe91309d5c07f542a6b72eb0d68e34919d
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-08-10 02:34:31 +0200

    revert dataloader tests

--- app/Services/DataLoader/Cache/CacheTest.php
+++ app/Services/DataLoader/Cache/CacheTest.php
@@ -214,6 +214,8 @@ class CacheTest extends TestCase {
     protected function item(): Model {
         return new class($this->faker->uuid, $this->faker->word) extends Model {
             public function __construct(string $key, string $property) {
+                parent::__construct();
+
                 $this->{$this->getKeyName()} = $key;
                 $this->property              = $property;
             }
