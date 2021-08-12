beaf2d9dddc5253053219cbd89f40d88df430882
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-12 15:07:23 +0400

    (tests) `WithSearch` improvements:
    + `createSearchIndex()`
    + `assertSearchIndexes()`
    + `assertSearchIndexExists()`
    + `assertSearchIndexAlias()`

--- tests/WithSearch.php
+++ tests/WithSearch.php
@@ -3,8 +3,6 @@
 namespace Tests;
 
 use App\Models\Model;
-use ElasticAdapter\Indices\Index;
-use ElasticAdapter\Indices\IndexManager;
 use Elasticsearch\Client;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Support\Collection;
@@ -16,6 +14,8 @@ use function str_starts_with;
  * @mixin \Tests\TestCase
  */
 trait WithSearch {
+    // <editor-fold desc="SetUp">
+    // =========================================================================
     protected function setUpWithSearch(): void {
         // Right now only ElasticSearch is supported.
         $this->setSettings([
@@ -35,11 +35,10 @@ trait WithSearch {
         }
 
         // Remove all indexes
-        $prefix  = $this->app->make(Repository::class)->get('scout.prefix');
         $indexes = (new Collection($client->indices()->getAlias()))
             ->keys()
-            ->filter(static function (string $index) use ($prefix): bool {
-                return str_starts_with($index, $prefix);
+            ->filter(function (string $index): bool {
+                return $this->isSearchName($index);
             })
             ->join(',');
 
@@ -51,20 +50,15 @@ trait WithSearch {
     protected function tearDownWithSearch(): void {
         // empty
     }
+    // </editor-fold>
 
+    // <editor-fold desc="Helpers">
+    // =========================================================================
     protected function makeSearchable(Collection|Model $models): Collection|Model {
         if ($models instanceof Model) {
-            // Add to index
             /** @var \App\Services\Search\Eloquent\Searchable $models */
+            $this->createSearchIndex($models->searchableAs());
             $models->searchable();
-
-            // Create index if not exists
-            $manager = $this->app->make(IndexManager::class);
-            $index   = $models->searchableAs();
-
-            if (!$manager->exists($index)) {
-                $manager->create(new Index($index));
-            }
         } else {
             // Foreach is used because Scout doesn't create an index right if the
             // collection contains models of different classes.
@@ -75,4 +69,119 @@ trait WithSearch {
 
         return $models;
     }
+
+    protected function createSearchIndex(string $index, string $alias = null): void {
+        $client = $this->app->make(Client::class)->indices();
+        $index  = $this->getSearchName($index);
+        $alias  = $this->getSearchName($alias);
+
+        if (!$client->exists(['index' => $index])) {
+            $client->create(['index' => $index]);
+        }
+
+        if ($alias) {
+            $client->updateAliases([
+                'body' => [
+                    'actions' => [
+                        // Remove alias from old index
+                        [
+                            'remove' => [
+                                'index' => '*',
+                                'alias' => $alias,
+                            ],
+                        ],
+                        // Add alias to new index
+                        [
+                            'add' => [
+                                'index'          => $index,
+                                'alias'          => $alias,
+                                'is_write_index' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+
+            $this->assertSearchIndexAlias($index, $alias);
+        } else {
+            $this->assertSearchIndexExists($index);
+        }
+    }
+
+    private function getSearchIndexPrefix(): string {
+        return $this->app->make(Repository::class)->get('scout.prefix');
+    }
+
+    private function getSearchName(?string $index): ?string {
+        if ($index !== null && !$this->isSearchName($index)) {
+            $index = "{$this->getSearchIndexPrefix()}{$index}";
+        }
+
+        return $index;
+    }
+
+    private function isSearchName(string $index): bool {
+        return str_starts_with($index, $this->getSearchIndexPrefix());
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Assertions">
+    // =========================================================================
+    /**
+     * @param array<mixed> $expected
+     */
+    protected function assertSearchIndexes(array $expected, string $message = ''): void {
+        // Prepare Expected
+        foreach ($expected as $index => $data) {
+            foreach ($data['aliases'] as $alias => $properties) {
+                if (!$this->isSearchName($alias)) {
+                    $expected[$index]['aliases'][$this->getSearchName($alias)] = $expected[$index]['aliases'][$alias];
+
+                    unset($expected[$index]['aliases'][$alias]);
+                }
+            }
+
+            if (!$this->isSearchName($index)) {
+                $expected[$this->getSearchName($index)] = $expected[$index];
+
+                unset($expected[$index]);
+            }
+        }
+
+        // Prepare Actual
+        $actual = $this->app->make(Client::class)->indices()->getAlias();
+
+        foreach ($actual as $index => $data) {
+            if (!$this->isSearchName($index)) {
+                unset($actual[$index]);
+            }
+        }
+
+        // Compare
+        $this->assertEquals($expected, $actual, $message);
+    }
+
+    protected function assertSearchIndexExists(string $expected, string $message = ''): void {
+        $this->assertTrue(
+            $this->app->make(Client::class)->indices()->exists([
+                'index' => $this->getSearchName($expected),
+            ]),
+            $message,
+        );
+    }
+
+    protected function assertSearchIndexAlias(
+        string $expectedIndex,
+        string $expectedAlias,
+        string $message = '',
+    ): void {
+        $this->assertNotEmpty(
+            $this->app->make(Client::class)->indices()->getAlias([
+                'name'  => $expectedAlias,
+                'index' => $expectedIndex,
+            ]),
+            $message,
+        );
+    }
+    // </editor-fold>
 }
