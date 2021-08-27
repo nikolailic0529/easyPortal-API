d2a3dcbee4f3dd401b52a51e97e46bdb6578a48c
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-27 09:15:38 +0400

    `OrganizationUpdater` will update KPIs.

--- app/Services/Organization/Listeners/OrganizationUpdater.php
+++ app/Services/Organization/Listeners/OrganizationUpdater.php
@@ -71,6 +71,7 @@ class OrganizationUpdater implements Subscriber {
         }
 
         // Update
+        $normalizer         = $this->normalizer;
         $organization->name = $reseller->name;
 
         if (isset($company->keycloakName)) {
@@ -83,7 +84,6 @@ class OrganizationUpdater implements Subscriber {
 
         if (isset($company->brandingData)) {
             $branding                                       = $company->brandingData;
-            $normalizer                                     = $this->normalizer;
             $organization->analytics_code                   = $normalizer->string($branding->resellerAnalyticsCode);
             $organization->branding_dark_theme              = $normalizer->boolean($branding->brandingMode);
             $organization->branding_main_color              = $normalizer->color($branding->mainColor);
@@ -97,6 +97,23 @@ class OrganizationUpdater implements Subscriber {
             $organization->branding_welcome_image_url       = $normalizer->string($branding->mainImageOnTheRight);
             $organization->branding_welcome_heading         = $normalizer->string($branding->mainHeadingText);
             $organization->branding_welcome_underline       = $normalizer->string($branding->underlineText);
+        }
+
+        if (isset($company->companyKpis)) {
+            $kpi                                       = $company->companyKpis;
+            $organization->kpi_assets_total            = (int) $normalizer->number($kpi->totalAssets);
+            $organization->kpi_assets_active           = (int) $normalizer->number($kpi->activeAssets);
+            $organization->kpi_assets_covered          = (float) $normalizer->number($kpi->activeAssetsPercentage);
+            $organization->kpi_customers_active        = (int) $normalizer->number($kpi->activeCustomers);
+            $organization->kpi_customers_active_new    = (int) $normalizer->number($kpi->newActiveCustomers);
+            $organization->kpi_contracts_active        = (int) $normalizer->number($kpi->activeContracts);
+            $organization->kpi_contracts_active_amount = (float) $normalizer->number($kpi->activeContractTotalAmount);
+            $organization->kpi_contracts_active_new    = (int) $normalizer->number($kpi->newActiveContracts);
+            $organization->kpi_contracts_expiring      = (int) $normalizer->number($kpi->expiringContracts);
+            $organization->kpi_quotes_active           = (int) $normalizer->number($kpi->activeQuotes);
+            $organization->kpi_quotes_active_amount    = (float) $normalizer->number($kpi->activeQuotesTotalAmount);
+            $organization->kpi_quotes_active_new       = (int) $normalizer->number($kpi->newActiveQuotes);
+            $organization->kpi_quotes_expiring         = (int) $normalizer->number($kpi->expiringQuotes);
         }
 
         // Save
