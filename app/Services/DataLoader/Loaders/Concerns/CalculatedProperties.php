037833951bcb705692240a70a96d4c7f19ac26dd
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-31 17:09:54 +0400

    `CalculatedProperties` will use `groupBy` to update countable properties of Customers.

--- app/Services/DataLoader/Loaders/Concerns/CalculatedProperties.php
+++ app/Services/DataLoader/Loaders/Concerns/CalculatedProperties.php
@@ -3,11 +3,15 @@
 namespace App\Services\DataLoader\Loaders\Concerns;
 
 use App\Models\Asset;
+use App\Models\Callbacks\GetKey;
 use App\Models\Customer;
 use App\Models\Document;
 use App\Models\Reseller;
 use App\Services\DataLoader\Resolver;
+use App\Services\DataLoader\Resolvers\CustomerResolver;
+use App\Utils\ModelHelper;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Throwable;
@@ -19,12 +23,32 @@ trait CalculatedProperties {
 
     protected function updateCalculatedProperties(Resolver ...$resolvers): void {
         foreach ($resolvers as $resolver) {
+            // Empty?
+            $objects = $resolver->getResolved();
+
+            if ($objects->isEmpty()) {
+                continue;
+            }
+
+            // Update
+            try {
+                if ($resolver instanceof CustomerResolver) {
+                    $this->updateCustomersCalculatedProperties($objects);
+
+                    continue;
+                }
+            } catch (Throwable $exception) {
+                $this->getLogger()->warning(__METHOD__, [
+                    'ids'       => $objects->map(new GetKey()),
+                    'resolver'  => $resolver::class,
+                    'exception' => $exception,
+                ]);
+            }
+
             foreach ($resolver->getResolved() as $object) {
                 try {
                     if ($object instanceof Reseller) {
                         $this->updateResellerCalculatedProperties($object);
-                    } elseif ($object instanceof Customer) {
-                        $this->updateCustomerCalculatedProperties($object);
                     } else {
                         throw new InvalidArgumentException(sprintf(
                             'Impossible to update calculated properties for `%s`.',
@@ -33,7 +57,7 @@ trait CalculatedProperties {
                     }
                 } catch (Throwable $exception) {
                     $this->getLogger()->warning(__METHOD__, [
-                        'reseller'  => $object,
+                        'object'    => $object,
                         'exception' => $exception,
                     ]);
                 }
@@ -41,9 +65,27 @@ trait CalculatedProperties {
         }
     }
 
-    protected function updateCustomerCalculatedProperties(Customer $customer): void {
-        $customer->assets_count = $customer->assets()->count();
-        $customer->save();
+    /**
+     * @param \Illuminate\Support\Collection<\App\Models\Customer> $customers
+     */
+    protected function updateCustomersCalculatedProperties(Collection $customers): void {
+        $customers  = $customers->keyBy(new GetKey());
+        $statistics = (new ModelHelper(Customer::query()))
+            ->getRelation('assets')
+            ->toBase()
+            ->select('customer_id', DB::raw('count(*) as assets_count'))
+            ->whereIn('customer_id', $customers->map(new GetKey())->all())
+            ->groupBy('customer_id')
+            ->get();
+
+        foreach ($statistics as $data) {
+            $customer = $customers[$data->customer_id];
+
+            if ($customer instanceof Customer) {
+                $customer->assets_count = $data->assets_count;
+                $customer->save();
+            }
+        }
     }
 
     protected function updateResellerCalculatedProperties(Reseller $reseller): void {
