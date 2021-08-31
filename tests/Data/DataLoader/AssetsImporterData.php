3829641d0b9925c54edb74c61a19e05fc2cc5014
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-31 09:35:45 +0400

    `AssetsImporterData` will generate `oem.csv`.

--- tests/Data/DataLoader/AssetsImporterData.php
+++ tests/Data/DataLoader/AssetsImporterData.php
@@ -12,13 +12,20 @@ use Illuminate\Console\Command;
 use function array_filter;
 use function array_unique;
 use function array_values;
+use function fclose;
+use function fopen;
+use function fputcsv;
+use function is_array;
+
+use const SORT_REGULAR;
 
 class AssetsImporterData extends Data {
+    public const LIMIT                = 50;
+    public const CHUNK                = 10;
     public const CONTEXT_DISTRIBUTORS = 'distributors';
     public const CONTEXT_RESELLERS    = 'resellers';
     public const CONTEXT_CUSTOMERS    = 'customers';
-    public const LIMIT                = 50;
-    public const CHUNK                = 10;
+    public const CONTEXT_OEMS         = 'oems';
 
     /**
      * @inheritDoc
@@ -49,6 +56,7 @@ class AssetsImporterData extends Data {
         $distributors = [];
         $resellers    = [];
         $customers    = [];
+        $oems         = [];
 
         foreach ((new ClientDumpsIterator($path))->getResponseIterator() as $object) {
             if ($object instanceof ViewAsset) {
@@ -59,6 +67,11 @@ class AssetsImporterData extends Data {
             } elseif ($object instanceof ViewAssetDocument) {
                 $resellers[] = $object->reseller->id ?? null;
                 $customers[] = $object->customer->id ?? null;
+                $oems[]      = [
+                    $object->document->vendorSpecificFields->vendor ?? null,
+                    $object->supportPackage ?? null,
+                    $object->skuNumber ?? null,
+                ];
             } elseif ($object instanceof ViewDocument) {
                 $resellers[]    = $object->resellerId ?? null;
                 $customers[]    = $object->customerId ?? null;
@@ -68,17 +81,55 @@ class AssetsImporterData extends Data {
             }
         }
 
+        // Store OEMs
+        $file = 'oem.csv';
+        $oems = array_values(array_filter(array_unique($oems, SORT_REGULAR)));
+        $csv  = fopen("{$path}/{$file}", 'w');
+
+        fputcsv($csv, [
+            'Vendor',
+            'Service Group SKU',
+            'Service Group Description',
+            'Service Level SKU',
+            'English',
+            'English',
+        ]);
+
+        foreach ($oems as $oem) {
+            [$oem, $group, $level] = $oem;
+
+            $oem   = $this->normalizer->string($oem);
+            $group = $this->normalizer->string($group);
+            $level = $this->normalizer->string($level);
+
+            if ($oem && $group && $level) {
+                fputcsv($csv, [
+                    $oem,
+                    $group,
+                    $this->faker->sentence,
+                    $level,
+                    $this->faker->sentence,
+                    $this->faker->text,
+                ]);
+            }
+        }
+
+        fclose($csv);
+
         // Cleanup
-        $extracted = [
+        $context = [
             static::CONTEXT_DISTRIBUTORS => $distributors,
             static::CONTEXT_RESELLERS    => $resellers,
             static::CONTEXT_CUSTOMERS    => $customers,
+            static::CONTEXT_OEMS         => $file,
         ];
 
-        foreach ($extracted as &$data) {
-            $data = array_values(array_filter(array_unique($data)));
+        foreach ($context as &$data) {
+            if (is_array($data)) {
+                $data = array_values(array_filter(array_unique($data, SORT_REGULAR)));
+            }
         }
 
-        return $extracted;
+        return $context;
     }
 }
