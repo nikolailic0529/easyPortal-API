1bfd06473b715eb57ecc7186e5f324194e42b685
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-02 16:34:30 +0400

    `AssetLoader` tests.

---
+++ app/Services/DataLoader/Loaders/AssetLoaderTest.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Loaders;
+
+use App\Models\Asset;
+use App\Models\AssetWarranty;
+use App\Models\Customer;
+use App\Models\Distributor;
+use App\Models\Document;
+use App\Models\DocumentEntry;
+use App\Models\Reseller;
+use App\Services\DataLoader\Testing\Helper;
+use LastDragon_ru\LaraASP\Testing\Database\QueryLog\WithQueryLog;
+use Tests\Data\Services\DataLoader\Loaders\AssetLoaderCreateWithDocuments;
+use Tests\Data\Services\DataLoader\Loaders\AssetLoaderCreateWithoutDocuments;
+use Tests\TestCase;
+
+use function count;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\Services\DataLoader\Loaders\AssetLoader
+ */
+class AssetLoaderTest extends TestCase {
+    use WithQueryLog;
+    use Helper;
+
+    /**
+     * @covers ::create
+     */
+    public function testCreateWithoutDocuments(): void {
+        // Generate
+        $this->generateData(AssetLoaderCreateWithoutDocuments::class);
+
+        // Pretest
+        $this->assertModelsCount([
+            Distributor::class   => 0,
+            Reseller::class      => 0,
+            Customer::class      => 1,
+            Asset::class         => 0,
+            AssetWarranty::class => 0,
+            Document::class      => 0,
+            DocumentEntry::class => 0,
+        ]);
+
+        // Test (cold)
+        $queries  = $this->getQueryLog();
+        $importer = $this->app->make(AssetLoader::class)
+            ->setWithDocuments(AssetLoaderCreateWithoutDocuments::DOCUMENTS);
+
+        $importer->create(AssetLoaderCreateWithoutDocuments::ASSET);
+
+        $actual   = $this->cleanupQueryLog($queries->get());
+        $expected = $this->getTestData()->json('~without-documents.json');
+
+        $this->assertCount(count($expected), $actual);
+        $this->assertEquals($expected, $actual);
+        $this->assertModelsCount([
+            Distributor::class   => 0,
+            Reseller::class      => 0,
+            Customer::class      => 1,
+            Asset::class         => 1,
+            AssetWarranty::class => 0,
+            Document::class      => 0,
+            DocumentEntry::class => 0,
+        ]);
+
+        $queries->flush();
+    }
+
+    /**
+     * @covers ::handle
+     */
+    public function testCreateWithDocuments(): void {
+        // Generate
+        $this->generateData(AssetLoaderCreateWithDocuments::class);
+
+        // Pretest
+        $this->assertModelsCount([
+            Distributor::class   => 1,
+            Reseller::class      => 1,
+            Customer::class      => 1,
+            Asset::class         => 0,
+            AssetWarranty::class => 0,
+            Document::class      => 0,
+            DocumentEntry::class => 0,
+        ]);
+
+        // Test (cold)
+        $queries  = $this->getQueryLog();
+        $importer = $this->app->make(AssetLoader::class)
+            ->setWithDocuments(AssetLoaderCreateWithDocuments::DOCUMENTS);
+
+        $importer->create(AssetLoaderCreateWithDocuments::ASSET);
+
+        $actual   = $this->cleanupQueryLog($queries->get());
+        $expected = $this->getTestData()->json('~with-documents.json');
+
+        $this->assertCount(count($expected), $actual);
+        $this->assertEquals($expected, $actual);
+        $this->assertModelsCount([
+            Distributor::class   => 1,
+            Reseller::class      => 1,
+            Customer::class      => 1,
+            Asset::class         => 1,
+            AssetWarranty::class => 6,
+            Document::class      => 4,
+            DocumentEntry::class => 16,
+        ]);
+
+        $queries->flush();
+    }
+}
