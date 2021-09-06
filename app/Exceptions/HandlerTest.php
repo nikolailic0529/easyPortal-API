960b501c25ca466a08c06f252251f8b88b9d17bf
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-06 14:05:03 +0400

    Added tests for `Handler::report()`.

--- app/Exceptions/HandlerTest.php
+++ app/Exceptions/HandlerTest.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Log\LogManager;
+use Mockery;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Tests\TestCase;
 use Throwable;
 
@@ -11,16 +15,25 @@ use Throwable;
  * @coversDefaultClass \App\Exceptions\Handler
  */
 class HandlerTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
     /**
      * @covers ::exceptionContext
      */
     public function testExceptionContext(): void {
-        $exception = new class() extends Exception implements Contextable {
+        $exception = new class('test') extends ApplicationException {
             /**
              * @inheritDoc
              */
             public function context(): array {
                 return [1, 2, 3];
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getContext(): array {
+                return [4, 5, 6];
             }
         };
         $handler   = new class() extends Handler {
@@ -37,6 +50,124 @@ class HandlerTest extends TestCase {
             }
         };
 
-        $this->assertEquals([1, 2, 3], $handler->exceptionContext($exception));
+        $this->assertEquals([1, 2, 3, 4, 5, 6], $handler->exceptionContext($exception));
+        $this->assertEquals([1, 2, 3, 4, 5, 6], $handler->getExceptionContext($exception));
     }
+
+    /**
+     * @covers ::report
+     *
+     * @dataProvider dataProviderReport
+     *
+     * @param array{level: string, channel: string, message: string, context: array<mixed>} $expected
+     */
+    public function testReport(array $expected, Throwable $exception): void {
+        $handler = $this->app->make(Handler::class);
+        $context = null;
+        $logger  = Mockery::mock(LoggerInterface::class);
+        $logger
+            ->shouldReceive('log')
+            ->with($expected['level'], $expected['message'], Mockery::any())
+            ->once()
+            ->andReturnUsing(static function (mixed $level, string $message, array $c) use (&$context): void {
+                $context = $c;
+            });
+
+        if ($exception instanceof ApplicationException) {
+            $manager = Mockery::mock(LogManager::class);
+            $manager
+                ->shouldReceive('channel')
+                ->with($expected['channel'])
+                ->once()
+                ->andReturn($logger);
+
+            $logger = $manager;
+        }
+
+        $this->override('log', static function () use ($logger): mixed {
+            return $logger;
+        });
+
+        $handler->report($exception);
+
+        unset($context['stack'][0]['line']);
+        unset($context['stack'][0]['trace']);
+
+        $this->assertEquals($expected['context'], $context);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProvider">
+    // =========================================================================
+    /**
+     * @return array<string,mixed>
+     */
+    public function dataProviderReport(): array {
+        $exception   = new class('test') extends Exception {
+            /**
+             * @inheritDoc
+             */
+            public function context(): array {
+                return [1, 2, 3];
+            }
+        };
+        $application = new class('test') extends ApplicationException {
+            public function getLevel(): ?string {
+                return LogLevel::ALERT;
+            }
+
+            public function getChannel(): ?string {
+                return 'test';
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getContext(): array {
+                return [1, 2, 3];
+            }
+        };
+
+        return [
+            'Exception'            => [
+                [
+                    'level'   => LogLevel::ERROR,
+                    'channel' => null,
+                    'message' => 'test',
+                    'context' => [
+                        'message' => 'Server Error.',
+                        'stack'   => [
+                            [
+                                'exception' => $exception::class,
+                                'message'   => 'test',
+                                'context'   => [1, 2, 3],
+                                'file'      => __FILE__,
+                            ],
+                        ],
+                    ],
+                ],
+                $exception,
+            ],
+            'ApplicationException' => [
+                [
+                    'level'   => $application->getLevel(),
+                    'channel' => $application->getChannel(),
+                    'message' => $application->getMessage(),
+                    'context' => [
+                        'message' => 'Server Error.',
+                        'stack'   => [
+                            [
+                                'exception' => $application::class,
+                                'message'   => 'test',
+                                'context'   => [1, 2, 3],
+                                'file'      => __FILE__,
+                            ],
+                        ],
+                    ],
+                ],
+                $application,
+            ],
+        ];
+    }
+    // </editor-fold>
 }
