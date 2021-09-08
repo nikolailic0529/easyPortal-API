e57134fa3809f61c44dd45daccc9fbce1539e07e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-08 16:03:38 +0400

    [Tokens] All exceptions will extend `ServiceException`.

--- app/Services/Tokens/Exceptions/InvalidCredentials.php
+++ app/Services/Tokens/Exceptions/InvalidCredentials.php
@@ -2,18 +2,19 @@
 
 namespace App\Services\Tokens\Exceptions;
 
+use App\Exceptions\TranslatedException;
+use App\Services\Tokens\ServiceException;
 use Throwable;
 
 use function __;
 
-class InvalidCredentials extends TokenException {
+class InvalidCredentials extends ServiceException implements TranslatedException {
     public function __construct(
         protected string $client,
         Throwable $previous = null,
     ) {
         parent::__construct(
             "Invalid credentials for `{$this->client}`.",
-            0,
             $previous,
         );
     }
