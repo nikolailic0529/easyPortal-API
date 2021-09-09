2b9b684c23b76665f8cb0ddc66d6689f56142ab2
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-09 11:57:24 +0400

    `ErrorCodes` tests and fixes.

--- app/Exceptions/ErrorCodes.php
+++ app/Exceptions/ErrorCodes.php
@@ -2,37 +2,40 @@
 
 namespace App\Exceptions;
 
-use App\GraphQL\Mutations\Auth\ResetPasswordSamePasswordException;
-use App\GraphQL\Mutations\Auth\SignUpByInviteAlreadyUsed;
-use App\GraphQL\Mutations\Auth\SignUpByInviteInvalidToken;
-use App\GraphQL\Mutations\Me\UpdateMePasswordInvalidCurrentPassword;
-use App\GraphQL\Mutations\Org\DisableOrgUserInvalidUser;
-use App\GraphQL\Mutations\Org\EnableOrgUserInvalidUser;
-use App\GraphQL\Mutations\Org\ResetOrgUserPasswordInvalidUser;
-use App\Http\Controllers\ExportGraphQLQueryEmpty;
-use App\Http\Controllers\ExportGraphQLQueryInvalid;
+use App\GraphQL\Mutations\Auth\ResetPasswordSamePasswordException as GraphQLResetPasswordSamePasswordException;
+use App\GraphQL\Mutations\Auth\SignUpByInviteAlreadyUsed as GraphQLSignUpByInviteAlreadyUsed;
+use App\GraphQL\Mutations\Auth\SignUpByInviteInvalidToken as GraphQLSignUpByInviteInvalidToken;
+use App\GraphQL\Mutations\Me\UpdateMePasswordInvalidCurrentPassword as GraphQLUpdateMePasswordInvalidCurrentPassword;
+use App\GraphQL\Mutations\Org\DisableOrgUserInvalidUser as GraphQLDisableOrgUserInvalidUser;
+use App\GraphQL\Mutations\Org\EnableOrgUserInvalidUser as GraphQLEnableOrgUserInvalidUser;
+use App\GraphQL\Mutations\Org\ResetOrgUserPasswordInvalidUser as GraphQLResetOrgUserPasswordInvalidUser;
+use App\Http\Controllers\ExportGraphQLQueryEmpty as HttpExportGraphQLQueryEmpty;
+use App\Http\Controllers\ExportGraphQLQueryInvalid as HttpExportGraphQLQueryInvalid;
 use App\Services\DataLoader\Client\Exceptions\DataLoaderDisabled as DataLoaderDataLoaderDisabled;
 use App\Services\DataLoader\Client\Exceptions\DataLoaderUnavailable as DataLoaderDataLoaderUnavailable;
 use App\Services\DataLoader\Client\Exceptions\GraphQLRequestFailed as DataLoaderGraphQLRequestFailed;
 use App\Services\Filesystem\Exceptions\StorageFileCorrupted as FilesystemStorageFileCorrupted;
 use App\Services\Filesystem\Exceptions\StorageFileDeleteFailed as FilesystemStorageFileDeleteFailed;
 use App\Services\Filesystem\Exceptions\StorageFileSaveFailed as FilesystemStorageFileSaveFailed;
-use App\Services\KeyCloak\Client\Exceptions\InvalidSettingClientUuid;
-use App\Services\KeyCloak\Client\Exceptions\KeyCloakDisabled;
-use App\Services\KeyCloak\Client\Exceptions\RealmGroupUnknown;
+use App\Services\KeyCloak\Client\Exceptions\InvalidSettingClientUuid as KeyCloakInvalidSettingClientUuid;
+use App\Services\KeyCloak\Client\Exceptions\KeyCloakDisabled as KeyCloakKeyCloakDisabled;
+use App\Services\KeyCloak\Client\Exceptions\KeyCloakUnavailable as KeyCloakKeyCloakUnavailable;
+use App\Services\KeyCloak\Client\Exceptions\RealmGroupUnknown as KeyCloakRealmGroupUnknown;
 use App\Services\KeyCloak\Client\Exceptions\RealmUserAlreadyExists as KeyCloakRealmUserAlreadyExists;
 use App\Services\KeyCloak\Client\Exceptions\RealmUserNotFound as KeyCloakRealmUserNotFound;
 use App\Services\KeyCloak\Client\Exceptions\RequestFailed as KeyCloakRequestFailed;
+use App\Services\KeyCloak\Client\Exceptions\ServerError as KeyCloakServerError;
 use App\Services\KeyCloak\Exceptions\AnotherUserExists as KeyCloakAnotherUserExists;
 use App\Services\KeyCloak\Exceptions\AuthorizationFailed as KeyCloakAuthorizationFailed;
 use App\Services\KeyCloak\Exceptions\InvalidCredentials as KeyCloakInvalidCredentials;
 use App\Services\KeyCloak\Exceptions\InvalidIdentity as KeyCloakInvalidIdentity;
 use App\Services\KeyCloak\Exceptions\StateMismatch as KeyCloakStateMismatch;
 use App\Services\KeyCloak\Exceptions\UnknownScope as KeyCloakUnknownScope;
-use App\Services\KeyCloak\Exceptions\UserDisabled;
+use App\Services\KeyCloak\Exceptions\UserDisabled as KeyCloakUserDisabled;
 use App\Services\KeyCloak\Exceptions\UserInsufficientData as KeyCloakUserInsufficientData;
 use App\Services\Organization\Exceptions\UnknownOrganization as OrganizationUnknownOrganization;
 use App\Services\Queue\Exceptions\ServiceNotFound as QueueServiceNotFound;
+use App\Services\Settings\Exceptions\FailedToLoadConfig as SettingsFailedToLoadConfig;
 use App\Services\Settings\Exceptions\FailedToLoadEnv as SettingsFailedToLoadEnv;
 use App\Services\Tokens\Exceptions\InvalidCredentials as TokensInvalidCredentials;
 use Throwable;
@@ -42,40 +45,67 @@ class ErrorCodes {
      * @var array<class-string<\Throwable>,string|int>
      */
     protected static array $map = [
-        QueueServiceNotFound::class                   => 'ERR01',
-        FilesystemStorageFileCorrupted::class         => 'ERR03',
-        FilesystemStorageFileDeleteFailed::class      => 'ERR04',
-        FilesystemStorageFileSaveFailed::class        => 'ERR05',
-        SettingsFailedToLoadEnv::class                => 'ERR06',
-        ExportGraphQLQueryInvalid::class              => 'ERR07',
-        KeyCloakAuthorizationFailed::class            => 'ERR08',
-        KeyCloakStateMismatch::class                  => 'ERR09',
-        KeyCloakInvalidIdentity::class                => 'ERR10',
-        KeyCloakInvalidCredentials::class             => 'ERR11',
-        KeyCloakUserInsufficientData::class           => 'ERR12',
-        OrganizationUnknownOrganization::class        => 'ERR13',
-        KeyCloakUnknownScope::class                   => 'ERR14',
-        KeyCloakAnotherUserExists::class              => 'ERR15',
-        TokensInvalidCredentials::class               => 'ERR16',
-        ExportGraphQLQueryEmpty::class                => 'ERR17',
-        DataLoaderDataLoaderDisabled::class           => 'ERR18',
-        DataLoaderDataLoaderUnavailable::class        => 'ERR19',
-        DataLoaderGraphQLRequestFailed::class         => 'ERR20',
-        KeyCloakRequestFailed::class                  => 'ERR21',
-        RealmGroupUnknown::class                      => 'ERR22',
-        KeyCloakDisabled::class                       => 'ERR23',
-        InvalidSettingClientUuid::class               => 'ERR24',
-        KeyCloakRealmUserAlreadyExists::class         => 'ERR25',
-        ResetPasswordSamePasswordException::class     => 'ERR27',
-        SignUpByInviteInvalidToken::class             => 'ERR28',
-        SignUpByInviteAlreadyUsed::class              => 'ERR31',
-        UpdateMePasswordInvalidCurrentPassword::class => 'ERR33',
-        ResetOrgUserPasswordInvalidUser::class        => 'ERR34',
-        KeyCloakRealmUserNotFound::class              => 'ERR36',
-        EnableOrgUserInvalidUser::class               => 'ERR37',
-        DisableOrgUserInvalidUser::class              => 'ERR38',
-        UserDisabled::class                           => 'ERR39',
+        // Http
+        HttpExportGraphQLQueryInvalid::class                 => 'Http001',
+        HttpExportGraphQLQueryEmpty::class                   => 'Http002',
+
+        // GraphQL
+        GraphQLResetPasswordSamePasswordException::class     => 'GraphQL001',
+        GraphQLSignUpByInviteInvalidToken::class             => 'GraphQL002',
+        GraphQLSignUpByInviteAlreadyUsed::class              => 'GraphQL003',
+        GraphQLUpdateMePasswordInvalidCurrentPassword::class => 'GraphQL004',
+        GraphQLResetOrgUserPasswordInvalidUser::class        => 'GraphQL005',
+        GraphQLEnableOrgUserInvalidUser::class               => 'GraphQL006',
+        GraphQLDisableOrgUserInvalidUser::class              => 'GraphQL007',
+
+        // Queue
+        QueueServiceNotFound::class                          => 'Queue001',
+
+        // Filesystem
+        FilesystemStorageFileCorrupted::class                => 'Filesystem001',
+        FilesystemStorageFileDeleteFailed::class             => 'Filesystem002',
+        FilesystemStorageFileSaveFailed::class               => 'Filesystem003',
+
+        // Settings
+        SettingsFailedToLoadEnv::class                       => 'Settings001',
+        SettingsFailedToLoadConfig::class                    => 'Settings002',
+
+        // KeyCloak
+        KeyCloakAuthorizationFailed::class                   => 'KeyCloak001',
+        KeyCloakStateMismatch::class                         => 'KeyCloak002',
+        KeyCloakInvalidIdentity::class                       => 'KeyCloak003',
+        KeyCloakInvalidCredentials::class                    => 'KeyCloak004',
+        KeyCloakUserInsufficientData::class                  => 'KeyCloak005',
+        KeyCloakUnknownScope::class                          => 'KeyCloak006',
+        KeyCloakAnotherUserExists::class                     => 'KeyCloak007',
+        KeyCloakRequestFailed::class                         => 'KeyCloak008',
+        KeyCloakRealmGroupUnknown::class                     => 'KeyCloak009',
+        KeyCloakKeyCloakDisabled::class                      => 'KeyCloak010',
+        KeyCloakInvalidSettingClientUuid::class              => 'KeyCloak011',
+        KeyCloakRealmUserAlreadyExists::class                => 'KeyCloak012',
+        KeyCloakRealmUserNotFound::class                     => 'KeyCloak013',
+        KeyCloakUserDisabled::class                          => 'KeyCloak014',
+        KeyCloakKeyCloakUnavailable::class                   => 'KeyCloak015',
+        KeyCloakServerError::class                           => 'KeyCloak016',
+
+        // Organization
+        OrganizationUnknownOrganization::class               => 'Organization001',
+
+        // Tokens
+        TokensInvalidCredentials::class                      => 'Tokens001',
+
+        // DataLoader
+        DataLoaderDataLoaderDisabled::class                  => 'DataLoader001',
+        DataLoaderDataLoaderUnavailable::class               => 'DataLoader002',
+        DataLoaderGraphQLRequestFailed::class                => 'DataLoader003',
     ];
+
+    /**
+     * @return array<class-string<\Throwable>,string|int>
+     */
+    public static function getMap(): array {
+        return self::$map;
+    }
 
     public static function getCode(Throwable $throwable): string|int {
         return self::$map[$throwable::class] ?? $throwable->getCode();
