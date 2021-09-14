71000e1bff7d62ee4b780cfcccbe9fca0cdcc8ee
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-14 11:44:57 +0400

    Added `DotenvRepository` for testings.

--- app/Services/Settings/Bootstrapers/LoadConfigurationTest.php
+++ app/Services/Settings/Bootstrapers/LoadConfigurationTest.php
@@ -3,7 +3,7 @@
 namespace App\Services\Settings\Bootstrapers;
 
 use App\Services\Settings\Config;
-use Dotenv\Repository\RepositoryInterface;
+use App\Services\Settings\Testings\DotenvRepository;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
 use Mockery;
@@ -80,45 +80,8 @@ class LoadConfigurationTest extends TestCase {
     public function testLoadEnvVars(): void {
         $app         = Mockery::mock(Application::class);
         $repository  = Mockery::mock(Repository::class);
-        $environment = new class(['FOO' => 'Foo']) implements RepositoryInterface {
-            /**
-             * @param array<string,mixed> $vars
-             */
-            public function __construct(
-                protected array $vars,
-            ) {
-                // empty
-            }
-
-            /**
-             * @return array<string,mixed>
-             */
-            public function getVars(): array {
-                return $this->vars;
-            }
-
-            public function has(string $name): bool {
-                return isset($this->vars[$name]);
-            }
-
-            public function get(string $name): mixed {
-                return $this->vars[$name] ?? null;
-            }
-
-            public function set(string $name, string $value): bool {
-                $this->vars[$name] = $value;
-
-                return true;
-            }
-
-            public function clear(string $name): bool {
-                $this->vars = [];
-
-                return true;
-            }
-        };
-
-        $config = Mockery::mock(Config::class);
+        $environment = new DotenvRepository(['FOO' => 'Foo']);
+        $config      = Mockery::mock(Config::class);
         $config
             ->shouldReceive('getEnvVars')
             ->once()
