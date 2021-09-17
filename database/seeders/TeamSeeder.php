fd5e9d8ddff5a21db63cfc1638262f6ea7a31e66
Mohamed Osama <mohammedosama.amagd@gmail.com>
2021-09-17 14:43:53 +0200

    TeamSeeder

---
+++ database/seeders/TeamSeeder.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use LastDragon_ru\LaraASP\Migrator\Seeders\SmartSeeder;
+
+class TeamSeeder extends SmartSeeder {
+    public function seed(): void {
+        $names = ['Marketing', 'Finance', 'Operations management', 'Human Resource', 'IT'];
+        foreach ($names as $name) {
+            $team       = new Team();
+            $team->key  = $name;
+            $team->name = $name;
+            $team->save();
+        }
+    }
+
+    protected function getTarget(): ?string {
+        return Team::class;
+    }
+}
