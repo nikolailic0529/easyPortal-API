6dc7465271a36c867d1911020be56ea017ee060d
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-17 16:31:00 +0400

    Migrations fix.

--- database/migrations/2021_09_17_105306_roles_organization_id_nullable~down.sql
+++ database/migrations/2021_09_17_105306_roles_organization_id_nullable~down.sql
@@ -3,9 +3,20 @@ SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 1;
 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 ALTER TABLE `roles`
+    DROP FOREIGN KEY `fk_roles_organizations1`;
+
+ALTER TABLE `roles`
     DROP INDEX `unique__name__organization_id__deleted_not`,
     DROP COLUMN `_organization_id`,
+    CHANGE COLUMN `organization_id` `organization_id` CHAR(36) NOT NULL AFTER `name`,
     ADD UNIQUE INDEX `unique__name__organization_id__deleted_not`(`name` ASC, `organization_id` ASC, `deleted_not` ASC) INVISIBLE;
+
+ALTER TABLE `roles`
+    ADD CONSTRAINT `fk_roles_organizations1`
+        FOREIGN KEY (`organization_id`)
+            REFERENCES `organizations`(`id`)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT;
 
 
 SET SQL_MODE = @OLD_SQL_MODE;
