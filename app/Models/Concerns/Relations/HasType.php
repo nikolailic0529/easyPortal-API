5c03245cd93ed4b1e68879f44c3a276f9505b940
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-24 09:45:25 +0400

    Removed unnecessary `HasType::type()`.

--- app/Models/Concerns/Relations/HasType.php
+++ app/Models/Concerns/Relations/HasType.php
@@ -3,7 +3,6 @@
 namespace App\Models\Concerns\Relations;
 
 use App\Models\Type;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @mixin \App\Models\Model
@@ -11,10 +10,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 trait HasType {
     use HasTypeNullable {
         setTypeAttribute as private setTypeAttributeNullable;
-    }
-
-    public function type(): BelongsTo {
-        return $this->belongsTo(Type::class);
     }
 
     public function setTypeAttribute(Type $type): void {
