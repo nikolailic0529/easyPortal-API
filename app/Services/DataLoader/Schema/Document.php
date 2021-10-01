f3777267f2ff090c9ddb3bf9c6f8003b88a684d8
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-01 10:50:28 +0400

    Updated Schema types for `Document` and `DocumentEntry`.

---
+++ app/Services/DataLoader/Schema/Document.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Schema;
+
+class Document extends Type {
+    public string                      $id;
+    public string                      $type;
+    public string                      $documentNumber;
+    public ?string                     $startDate;
+    public ?string                     $endDate;
+    public ?string                     $currencyCode;
+    public ?string                     $totalNetPrice;
+    public ?string                     $languageCode;
+    public ?string                     $updatedAt;
+    public ?string                     $resellerId;
+    public ?string                     $customerId;
+    public ?string                     $distributorId;
+    public DocumentVendorSpecificField $vendorSpecificFields;
+
+    /**
+     * @var array<\App\Services\DataLoader\Schema\CompanyContactPerson>
+     */
+    public ?array $contactPersons;
+
+    /**
+     * @var array<\App\Services\DataLoader\Schema\DocumentEntry>
+     */
+    public ?array $documentEntries;
+
+    /**
+     * @var array<string>|null
+     */
+    public ?array $status;
+}
---
+++ app/Services/DataLoader/Schema/DocumentTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Schema;
+
+use Tests\TestCase;
+
+use function array_keys;
+use function json_encode;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\Services\DataLoader\Schema\Document
+ */
+class DocumentTest extends TestCase {
+    /**
+     * @covers ::create
+     */
+    public function testCreate(): void {
+        $json       = $this->getTestData()->json();
+        $actual     = new Document($json);
+        $properties = Document::getPropertiesNames();
+
+        $this->assertEquals(array_keys($json), $properties);
+        $this->assertInstanceOf(DocumentVendorSpecificField::class, $actual->vendorSpecificFields);
+        $this->assertInstanceOf(CompanyContactPerson::class, $actual->contactPersons[0]);
+        $this->assertInstanceOf(DocumentEntry::class, $actual->documentEntries[0]);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($json),
+            json_encode($actual),
+        );
+    }
+}
