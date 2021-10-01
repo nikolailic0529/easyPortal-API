775708bbff65955ee870980d3dd36982b6fce306
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-01 16:05:32 +0400

    Added `WithAsset` trait.

---
+++ app/Services/DataLoader/Factories/Concerns/WithAssetTest.php
@@ -0,0 +1,162 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Factories\Concerns;
+
+use App\Models\Asset;
+use App\Services\DataLoader\Exceptions\AssetNotFound;
+use App\Services\DataLoader\Factory;
+use App\Services\DataLoader\Finders\AssetFinder;
+use App\Services\DataLoader\Normalizer;
+use App\Services\DataLoader\Resolvers\AssetResolver;
+use App\Services\DataLoader\Schema\Document;
+use App\Services\DataLoader\Schema\DocumentEntry;
+use App\Services\DataLoader\Schema\ViewAsset;
+use Closure;
+use Mockery;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\Services\DataLoader\Factories\Concerns\WithAsset
+ */
+class WithAssetTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::asset
+     *
+     * @dataProvider dataProviderAsset
+     */
+    public function testAssetExistsThroughProvider(Closure $objectFactory): void {
+        $normalizer = $this->app->make(Normalizer::class);
+        $asset      = Asset::factory()->make();
+        $resolver   = Mockery::mock(AssetResolver::class);
+        $resolver
+            ->shouldReceive('get')
+            ->with($asset->getKey(), Mockery::any())
+            ->once()
+            ->andReturn($asset);
+
+        $factory = new WithAssetTestObject($normalizer, $resolver);
+        $object  = $objectFactory($this, $asset);
+
+        $this->assertEquals($asset, $factory->asset($object));
+    }
+
+    /**
+     * @covers ::asset
+     *
+     * @dataProvider dataProviderAsset
+     */
+    public function testAssetExistsThroughFinder(Closure $objectFactory): void {
+        $normalizer = $this->app->make(Normalizer::class);
+        $asset      = Asset::factory()->make();
+        $resolver   = Mockery::mock(AssetResolver::class, [$this->app->make(Normalizer::class)]);
+        $resolver->shouldAllowMockingProtectedMethods();
+        $resolver->makePartial();
+        $resolver
+            ->shouldReceive('find')
+            ->with($asset->getKey())
+            ->once()
+            ->andReturn(null);
+        $finder = Mockery::mock(AssetFinder::class);
+        $finder
+            ->shouldReceive('find')
+            ->with($asset->getKey())
+            ->once()
+            ->andReturn($asset);
+
+        $factory = new WithAssetTestObject($normalizer, $resolver, $finder);
+        $object  = $objectFactory($this, $asset);
+
+        $this->assertEquals($asset, $factory->asset($object));
+    }
+
+    /**
+     * @covers ::asset
+     *
+     * @dataProvider dataProviderAsset
+     */
+    public function testAssetAssetNotFound(Closure $objectFactory): void {
+        $normalizer = $this->app->make(Normalizer::class);
+        $asset      = Asset::factory()->make();
+        $resolver   = Mockery::mock(AssetResolver::class);
+        $resolver
+            ->shouldReceive('get')
+            ->with($asset->getKey(), Mockery::any())
+            ->once()
+            ->andReturn(null);
+
+        $factory = new WithAssetTestObject($normalizer, $resolver);
+        $object  = $objectFactory($this, $asset);
+
+        $this->expectException(AssetNotFound::class);
+
+        $this->assertEquals($asset, $factory->asset($object));
+    }
+
+    /**
+     * @covers ::asset
+     */
+    public function testAssetAssetWithoutAsset(): void {
+        $normalizer = $this->app->make(Normalizer::class);
+        $object     = new DocumentEntry();
+        $resolver   = Mockery::mock(AssetResolver::class);
+        $resolver
+            ->shouldReceive('get')
+            ->never();
+
+        $factory = new WithAssetTestObject($normalizer, $resolver);
+
+        $this->assertNull($factory->asset($object));
+    }
+    //</editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderAsset(): array {
+        return [
+            DocumentEntry::class => [
+                static function (TestCase $test, Asset $asset) {
+                    return new DocumentEntry([
+                        'assetId' => $asset->getKey(),
+                    ]);
+                },
+            ],
+        ];
+    }
+    // </editor-fold>
+}
+
+// @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+// @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+/**
+ * @internal
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class WithAssetTestObject extends Factory {
+    use WithAsset {
+        asset as public;
+    }
+
+    /** @noinspection PhpMissingParentConstructorInspection */
+    public function __construct(
+        protected Normalizer $normalizer,
+        protected AssetResolver $assetResolver,
+        protected ?AssetFinder $assetFinder = null,
+    ) {
+        // empty
+    }
+
+    protected function getAssetResolver(): AssetResolver {
+        return $this->assetResolver;
+    }
+
+    protected function getAssetFinder(): ?AssetFinder {
+        return $this->assetFinder;
+    }
+}
