6e8dfe7369806f6a36606f8feb6836d0e8a3a62d
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-06 15:33:59 +0400

    [GraphQL] Updated permissions for Customers queries: `customersTypes`, `customersStatuses`.

--- app/GraphQL/Queries/Customers/CustomerStatusesTest.php
+++ app/GraphQL/Queries/Customers/CustomerStatusesTest.php
@@ -9,8 +9,9 @@ use Illuminate\Translation\Translator;
 use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\MergeDataProvider;
 use Tests\DataProviders\GraphQL\Organizations\OrganizationDataProvider;
-use Tests\DataProviders\GraphQL\Users\UserDataProvider;
+use Tests\DataProviders\GraphQL\Users\OrganizationUserDataProvider;
 use Tests\GraphQL\GraphQLSuccess;
 use Tests\TestCase;
 
@@ -61,64 +62,70 @@ class CustomerStatusesTest extends TestCase {
      * @return array<mixed>
      */
     public function dataProviderInvoke(): array {
-        return (new CompositeDataProvider(
-            new OrganizationDataProvider('customerStatuses'),
-            new UserDataProvider('customerStatuses'),
-            new ArrayDataProvider([
-                'ok' => [
-                    new GraphQLSuccess('customerStatuses', CustomerStatuses::class, [
-                        [
-                            'id'   => '6f19ef5f-5963-437e-a798-29296db08d59',
-                            'name' => 'Translated (locale)',
-                        ],
-                        [
-                            'id'   => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
-                            'name' => 'Translated (fallback)',
-                        ],
-                        [
-                            'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name' => 'No translation',
-                        ],
-                    ]),
-                    static function (TestCase $test): string {
-                        $translator = $test->app()->make(Translator::class);
-                        $fallback   = $translator->getFallback();
-                        $locale     = $test->app()->getLocale();
-                        $model      = (new Status())->getMorphClass();
+        $provider = new ArrayDataProvider([
+            'ok' => [
+                new GraphQLSuccess('customerStatuses', CustomerStatuses::class, [
+                    [
+                        'id'   => '6f19ef5f-5963-437e-a798-29296db08d59',
+                        'name' => 'Translated (locale)',
+                    ],
+                    [
+                        'id'   => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
+                        'name' => 'Translated (fallback)',
+                    ],
+                    [
+                        'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                        'name' => 'No translation',
+                    ],
+                ]),
+                static function (TestCase $test): string {
+                    $translator = $test->app()->make(Translator::class);
+                    $fallback   = $translator->getFallback();
+                    $locale     = $test->app()->getLocale();
+                    $model      = (new Status())->getMorphClass();
 
-                        $translator->addLines([
-                            "models.{$model}.6f19ef5f-5963-437e-a798-29296db08d59.name" => 'Translated (locale)',
-                        ], $locale);
+                    $translator->addLines([
+                        "models.{$model}.6f19ef5f-5963-437e-a798-29296db08d59.name" => 'Translated (locale)',
+                    ], $locale);
 
-                        $translator->addLines([
-                            "models.{$model}.f3cb1fac-b454-4f23-bbb4-f3d84a1699ae.name" => 'Translated (fallback)',
-                        ], $fallback);
+                    $translator->addLines([
+                        "models.{$model}.f3cb1fac-b454-4f23-bbb4-f3d84a1699ae.name" => 'Translated (fallback)',
+                    ], $fallback);
 
-                        return $locale;
-                    },
-                    static function (TestCase $test): void {
-                        Status::factory()->create([
-                            'id'          => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name'        => 'No translation',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Status::factory()->create([
-                            'id'          => '6f19ef5f-5963-437e-a798-29296db08d59',
-                            'name'        => 'Should be translated',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Status::factory()->create([
-                            'id'          => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
-                            'name'        => 'Should be translated via fallback',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Status::factory()->create([
-                            'name' => 'Wrong object_type',
-                        ]);
-                    },
-                ],
-            ]),
-        ))->getData();
+                    return $locale;
+                },
+                static function (TestCase $test): void {
+                    Status::factory()->create([
+                        'id'          => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                        'name'        => 'No translation',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Status::factory()->create([
+                        'id'          => '6f19ef5f-5963-437e-a798-29296db08d59',
+                        'name'        => 'Should be translated',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Status::factory()->create([
+                        'id'          => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
+                        'name'        => 'Should be translated via fallback',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Status::factory()->create([
+                        'name' => 'Wrong object_type',
+                    ]);
+                },
+            ],
+        ]);
+
+        return (new MergeDataProvider([
+            'customers-view' => new CompositeDataProvider(
+                new OrganizationDataProvider('customerStatuses'),
+                new OrganizationUserDataProvider('customerStatuses', [
+                    'customers-view',
+                ]),
+                $provider,
+            ),
+        ]))->getData();
     }
     // </editor-fold>
 }
--- app/GraphQL/Queries/Customers/CustomerTypesTest.php
+++ app/GraphQL/Queries/Customers/CustomerTypesTest.php
@@ -9,8 +9,9 @@ use Illuminate\Translation\Translator;
 use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\MergeDataProvider;
 use Tests\DataProviders\GraphQL\Organizations\OrganizationDataProvider;
-use Tests\DataProviders\GraphQL\Users\UserDataProvider;
+use Tests\DataProviders\GraphQL\Users\OrganizationUserDataProvider;
 use Tests\GraphQL\GraphQLSuccess;
 use Tests\TestCase;
 
@@ -61,64 +62,70 @@ class CustomerTypesTest extends TestCase {
      * @return array<mixed>
      */
     public function dataProviderInvoke(): array {
-        return (new CompositeDataProvider(
-            new OrganizationDataProvider('customerTypes'),
-            new UserDataProvider('customerTypes'),
-            new ArrayDataProvider([
-                'ok' => [
-                    new GraphQLSuccess('customerTypes', CustomerTypes::class, [
-                        [
-                            'id'   => '6f19ef5f-5963-437e-a798-29296db08d59',
-                            'name' => 'Translated (locale)',
-                        ],
-                        [
-                            'id'   => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
-                            'name' => 'Translated (fallback)',
-                        ],
-                        [
-                            'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name' => 'No translation',
-                        ],
-                    ]),
-                    static function (TestCase $test): string {
-                        $translator = $test->app()->make(Translator::class);
-                        $fallback   = $translator->getFallback();
-                        $locale     = $test->app()->getLocale();
-                        $model      = (new Type())->getMorphClass();
+        $provider = new ArrayDataProvider([
+            'ok' => [
+                new GraphQLSuccess('customerTypes', CustomerTypes::class, [
+                    [
+                        'id'   => '6f19ef5f-5963-437e-a798-29296db08d59',
+                        'name' => 'Translated (locale)',
+                    ],
+                    [
+                        'id'   => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
+                        'name' => 'Translated (fallback)',
+                    ],
+                    [
+                        'id'   => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                        'name' => 'No translation',
+                    ],
+                ]),
+                static function (TestCase $test): string {
+                    $translator = $test->app()->make(Translator::class);
+                    $fallback   = $translator->getFallback();
+                    $locale     = $test->app()->getLocale();
+                    $model      = (new Type())->getMorphClass();
 
-                        $translator->addLines([
-                            "models.{$model}.6f19ef5f-5963-437e-a798-29296db08d59.name" => 'Translated (locale)',
-                        ], $locale);
+                    $translator->addLines([
+                        "models.{$model}.6f19ef5f-5963-437e-a798-29296db08d59.name" => 'Translated (locale)',
+                    ], $locale);
 
-                        $translator->addLines([
-                            "models.{$model}.f3cb1fac-b454-4f23-bbb4-f3d84a1699ae.name" => 'Translated (fallback)',
-                        ], $fallback);
+                    $translator->addLines([
+                        "models.{$model}.f3cb1fac-b454-4f23-bbb4-f3d84a1699ae.name" => 'Translated (fallback)',
+                    ], $fallback);
 
-                        return $locale;
-                    },
-                    static function (TestCase $test): void {
-                        Type::factory()->create([
-                            'id'          => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
-                            'name'        => 'No translation',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Type::factory()->create([
-                            'id'          => '6f19ef5f-5963-437e-a798-29296db08d59',
-                            'name'        => 'Should be translated',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Type::factory()->create([
-                            'id'          => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
-                            'name'        => 'Should be translated via fallback',
-                            'object_type' => (new Customer())->getMorphClass(),
-                        ]);
-                        Type::factory()->create([
-                            'name' => 'Wrong object_type',
-                        ]);
-                    },
-                ],
-            ]),
-        ))->getData();
+                    return $locale;
+                },
+                static function (TestCase $test): void {
+                    Type::factory()->create([
+                        'id'          => 'f9396bc1-2f2f-4c57-bb8d-7a224ac20944',
+                        'name'        => 'No translation',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Type::factory()->create([
+                        'id'          => '6f19ef5f-5963-437e-a798-29296db08d59',
+                        'name'        => 'Should be translated',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Type::factory()->create([
+                        'id'          => 'f3cb1fac-b454-4f23-bbb4-f3d84a1699ae',
+                        'name'        => 'Should be translated via fallback',
+                        'object_type' => (new Customer())->getMorphClass(),
+                    ]);
+                    Type::factory()->create([
+                        'name' => 'Wrong object_type',
+                    ]);
+                },
+            ],
+        ]);
+
+        return (new MergeDataProvider([
+            'customers-view' => new CompositeDataProvider(
+                new OrganizationDataProvider('customerTypes'),
+                new OrganizationUserDataProvider('customerTypes', [
+                    'customers-view',
+                ]),
+                $provider,
+            ),
+        ]))->getData();
     }
     // </editor-fold>
 }
