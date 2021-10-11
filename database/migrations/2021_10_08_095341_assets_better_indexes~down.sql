42351dd8a993ce46faa0dc7a875978b808089a91
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-11 10:15:31 +0400

    Mode indexes for `assets`, `asset_coverages`, `asset_warranties`, `asset_tags`, `document_entries`.

--- database/migrations/2021_10_08_095341_assets_better_indexes~down.sql
+++ database/migrations/2021_10_08_095341_assets_better_indexes~down.sql
@@ -3,10 +3,28 @@ SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 1;
 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 ALTER TABLE `assets`
-    DROP INDEX `idx__deleted_at`;
+    DROP INDEX `idx__deleted_at`,
+    DROP INDEX `idx__product_id__deleted_at`,
+    DROP INDEX `idx__reseller_id__deleted_at`,
+    DROP INDEX `idx__customer_id__deleted_at`,
+    DROP INDEX `idx__location_id__deleted_at`,
+    DROP INDEX `idx__status_id__deleted_at`,
+    DROP INDEX `idx__type_id__deleted_at`;
 
 ALTER TABLE `asset_coverages`
+    DROP INDEX `idx__asset_id__deleted_at`,
     DROP INDEX `idx__coverage_id__deleted_at`;
+
+ALTER TABLE `asset_warranties`
+    DROP INDEX `idx__asset_id__deleted_at`,
+    DROP INDEX `idx__asset_id__end__deleted_at`;
+
+ALTER TABLE `asset_tags`
+    DROP INDEX `idx__tag_id__deleted_at`,
+    DROP INDEX `idx__asset_id__deleted_at`;
+
+ALTER TABLE `document_entries`
+    DROP INDEX `idx__document_id__deleted_at`;
 
 
 SET SQL_MODE = @OLD_SQL_MODE;
--- database/migrations/2021_10_08_095341_assets_better_indexes~up.sql
+++ database/migrations/2021_10_08_095341_assets_better_indexes~up.sql
@@ -3,10 +3,28 @@ SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 1;
 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 ALTER TABLE `assets`
-    ADD INDEX `idx__deleted_at`(`deleted_at` ASC) VISIBLE;
+    ADD INDEX `idx__deleted_at`(`deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__product_id__deleted_at` (`product_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__reseller_id__deleted_at` (`reseller_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__customer_id__deleted_at` (`customer_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__location_id__deleted_at` (`location_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__status_id__deleted_at` (`status_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__type_id__deleted_at` (`type_id` ASC, `deleted_at` ASC) VISIBLE;
 
 ALTER TABLE `asset_coverages`
+    ADD INDEX `idx__asset_id__deleted_at` (`asset_id` ASC, `deleted_at` ASC) VISIBLE,
     ADD INDEX `idx__coverage_id__deleted_at`(`coverage_id` ASC, `deleted_at` ASC) VISIBLE;
+
+ALTER TABLE `asset_warranties`
+    ADD INDEX `idx__asset_id__deleted_at` (`asset_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__asset_id__end__deleted_at` (`asset_id` ASC, `end` ASC, `deleted_at` ASC) VISIBLE;
+
+ALTER TABLE `asset_tags`
+    ADD INDEX `idx__tag_id__deleted_at` (`tag_id` ASC, `deleted_at` ASC) VISIBLE,
+    ADD INDEX `idx__asset_id__deleted_at` (`asset_id` ASC, `deleted_at` ASC) VISIBLE;
+
+ALTER TABLE `document_entries`
+    ADD INDEX `idx__document_id__deleted_at` (`document_id` ASC, `deleted_at` ASC) VISIBLE;
 
 
 SET SQL_MODE = @OLD_SQL_MODE;
