16292d588f181c3b9534ff8bc7adfcbd56760097
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-18 09:53:07 +0400

    Migration will not set `synced_at` (= it will have default value).

--- database/migrations/2021_10_15_122534_object_synched_at~up.sql
+++ database/migrations/2021_10_15_122534_object_synched_at~up.sql
@@ -5,32 +5,17 @@ SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABL
 ALTER TABLE `assets`
     ADD COLUMN `synced_at` TIMESTAMP NOT NULL AFTER `changed_at`;
 
-UPDATE `assets`
-SET `changed_at` = `updated_at`;
-
 ALTER TABLE `customers`
     ADD COLUMN `synced_at` TIMESTAMP NOT NULL AFTER `changed_at`;
-
-UPDATE `customers`
-SET `changed_at` = `updated_at`;
 
 ALTER TABLE `documents`
     ADD COLUMN `synced_at` TIMESTAMP NOT NULL AFTER `changed_at`;
 
-UPDATE `documents`
-SET `changed_at` = `updated_at`;
-
 ALTER TABLE `resellers`
     ADD COLUMN `synced_at` TIMESTAMP NOT NULL AFTER `changed_at`;
 
-UPDATE `resellers`
-SET `changed_at` = `updated_at`;
-
 ALTER TABLE `distributors`
     ADD COLUMN `synced_at` TIMESTAMP NOT NULL AFTER `changed_at`;
-
-UPDATE `distributors`
-SET `changed_at` = `updated_at`;
 
 
 SET SQL_MODE = @OLD_SQL_MODE;
