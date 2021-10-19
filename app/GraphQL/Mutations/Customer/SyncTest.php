d16a923e047e427eee24b614bf6730ad8bf09467
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-19 13:51:40 +0400

    Added permission to `customer.sync()` mutation.

--- app/GraphQL/Mutations/Customer/SyncTest.php
+++ app/GraphQL/Mutations/Customer/SyncTest.php
@@ -2,19 +2,25 @@
 
 namespace App\GraphQL\Mutations\Customer;
 
+use App\Models\Customer;
+use App\Models\Organization;
+use App\Models\Reseller;
+use App\Models\User;
 use App\Services\DataLoader\Jobs\CustomerSync;
 use Closure;
 use Illuminate\Support\Facades\Queue;
 use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
-use Tests\DataProviders\GraphQL\Organizations\RootOrganizationDataProvider;
-use Tests\DataProviders\GraphQL\Users\RootUserDataProvider;
+use Tests\DataProviders\GraphQL\Organizations\OrganizationDataProvider;
+use Tests\DataProviders\GraphQL\Users\OrganizationUserDataProvider;
+use Tests\GraphQL\GraphQLError;
 use Tests\GraphQL\GraphQLSuccess;
 use Tests\GraphQL\JsonFragment;
 use Tests\GraphQL\JsonFragmentSchema;
 use Tests\TestCase;
 
+use function __;
 use function array_filter;
 use function count;
 
@@ -37,8 +43,33 @@ class SyncTest extends TestCase {
         Closure $organizationFactory,
         Closure $userFactory = null,
         array $input = [],
+        Closure $prepare = null,
     ): void {
-        $this->setUser($userFactory, $this->setOrganization($organizationFactory));
+        $organization = $this->setOrganization($organizationFactory);
+        $user         = $this->setUser($userFactory, $organization);
+        $id           = $this->faker->uuid;
+
+        if ($prepare) {
+            $prepare($this, $organization, $user, $input);
+        } else {
+            // Lighthouse performs validation BEFORE permission check :(
+            //
+            // https://github.com/nuwave/lighthouse/issues/1780
+            //
+            // Following code required to "fix" it
+            if (!$organization) {
+                $organization = $this->setOrganization(Organization::factory()->create());
+            }
+
+            $reseller = Reseller::factory()->create([
+                'id' => $organization ? $organization->getKey() : $this->faker->uuid,
+            ]);
+            $customer = Customer::factory()->create([
+                'id' => $id,
+            ]);
+
+            $reseller->customers()->attach($customer);
+        }
 
         Queue::fake();
 
@@ -54,7 +85,7 @@ class SyncTest extends TestCase {
                     }
                 }',
                 [
-                    'input' => $input ?: [['id' => '8b79a366-f9c2-4eb1-b8e5-5423bc333f96']],
+                    'input' => $input ?: [['id' => $id]],
                 ],
             )
             ->assertThat($expected);
@@ -87,9 +118,25 @@ class SyncTest extends TestCase {
      * @return array<mixed>
      */
     public function dataProviderInvoke(): array {
+        $factory = static function (TestCase $test, Organization $organization, User $user, array $input): void {
+            $reseller = Reseller::factory()->create([
+                'id' => $organization->getKey(),
+            ]);
+
+            foreach ($input as $call) {
+                $customer = Customer::factory()->create([
+                    'id' => $call['id'],
+                ]);
+
+                $reseller->customers()->attach($customer);
+            }
+        };
+
         return (new CompositeDataProvider(
-            new RootOrganizationDataProvider('customer'),
-            new RootUserDataProvider('customer'),
+            new OrganizationDataProvider('customer'),
+            new  OrganizationUserDataProvider('customer', [
+                'customers-sync',
+            ]),
             new ArrayDataProvider([
                 'ok'                             => [
                     new GraphQLSuccess(
@@ -107,6 +154,7 @@ class SyncTest extends TestCase {
                             'id' => 'd840dfdb-7c9a-4324-8470-12ec91199834',
                         ],
                     ],
+                    $factory,
                 ],
                 'ok (with assets)'               => [
                     new GraphQLSuccess(
@@ -122,6 +170,7 @@ class SyncTest extends TestCase {
                             'assets' => true,
                         ],
                     ],
+                    $factory,
                 ],
                 'ok (with assets and documents)' => [
                     new GraphQLSuccess(
@@ -138,6 +187,20 @@ class SyncTest extends TestCase {
                             'documents' => true,
                         ],
                     ],
+                    $factory,
+                ],
+                'invalid customer'               => [
+                    new GraphQLError('customer', static function (): array {
+                        return [__('errors.validation_failed')];
+                    }),
+                    [
+                        [
+                            'id' => 'd2ff874e-5c60-4016-a6d0-f0b970b38b17',
+                        ],
+                    ],
+                    static function (): void {
+                        // empty
+                    },
                 ],
             ]),
         ))->getData();
