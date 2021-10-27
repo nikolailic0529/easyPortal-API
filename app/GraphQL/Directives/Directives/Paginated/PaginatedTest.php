3f5c48d3fb58585b65cd2ed6bf6e84e629dc1057
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-10-27 11:32:38 +0400

    `@paginated`: base code extracted to base class.

--- app/GraphQL/Directives/Directives/Paginated/PaginatedTest.php
+++ app/GraphQL/Directives/Directives/Paginated/PaginatedTest.php
@@ -2,17 +2,10 @@
 
 namespace App\GraphQL\Directives\Directives\Paginated;
 
-use App\GraphQL\Directives\Definitions\PaginatedDirective;
 use App\GraphQL\Queries\Customers\Customer;
 use App\Models\Asset;
-use App\Services\Search\Builders\Builder as SearchBuilder;
-use Closure;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
-use Illuminate\Database\Eloquent\Model;
-use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
-use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
 use LastDragon_ru\LaraASP\Testing\Responses\Laravel\Json\OkResponse;
-use Tests\DataProviders\Builders\BuilderDataProvider;
 use Tests\GraphQL\Schemas\AnySchema;
 use Tests\TestCase;
 use Tests\WithGraphQLSchema;
@@ -47,50 +40,6 @@ class PaginatedTest extends TestCase {
             $this->getGraphQLSchemaExpected($expected, '~schema.graphql'),
             $this->getTestData()->content('~schema.graphql'),
         );
-    }
-
-    /**
-     * @covers ::handleBuilder
-     *
-     * @dataProvider dataProviderHandleBuilder
-     *
-     * @param array{query: string, bindings: array<mixed>} $expected
-     * @param array<mixed>                                 $args
-     */
-    public function testHandleBuilder(
-        array $expected,
-        Closure $builder,
-        array $args,
-    ): void {
-        $directive = $this->app->make(PaginatedDirective::class);
-        $builder   = $builder($this);
-        $builder   = $directive->handleBuilder($builder, $args);
-
-        $this->assertDatabaseQueryEquals($expected, $builder);
-    }
-
-    /**
-     * @covers ::handleScoutBuilder
-     *
-     * @dataProvider dataProviderHandleScoutBuilder
-     *
-     * @param array{limit: ?int, offset: int} $expected
-     * @param array<mixed>                    $args
-     */
-    public function testHandleScoutBuilder(array $expected, array $args): void {
-        $directive = $this->app->make(PaginatedDirective::class);
-        $builder   = $this->app->make(SearchBuilder::class, [
-            'query' => '123',
-            'model' => new class() extends Model {
-                // empty
-            },
-        ]);
-        $builder   = $directive->handleScoutBuilder($builder, $args);
-
-        $this->assertEquals($expected, [
-            'limit'  => $builder->limit,
-            'offset' => $builder->offset ?? null,
-        ]);
     }
 
     /**
@@ -250,7 +199,7 @@ class PaginatedTest extends TestCase {
                 ],
             ]));
     }
-    // </editor-fold>
+    //</editor-fold>
 
     // <editor-fold desc="DataProviders">
     // =========================================================================
@@ -275,100 +224,7 @@ class PaginatedTest extends TestCase {
             ],
         ];
     }
-
-    /**
-     * @return array<string,mixed>
-     */
-    public function dataProviderHandleBuilder(): array {
-        return (new CompositeDataProvider(
-            new BuilderDataProvider(),
-            new ArrayDataProvider([
-                'limit + offset' => [
-                    [
-                        'query'    => 'select * from `tmp` limit 123 offset 45',
-                        'bindings' => [],
-                    ],
-                    [
-                        'limit'  => 123,
-                        'offset' => 45,
-                    ],
-                ],
-                'limit'          => [
-                    [
-                        'query'    => 'select * from `tmp` limit 123',
-                        'bindings' => [],
-                    ],
-                    [
-                        'limit' => 123,
-                    ],
-                ],
-                'offset'         => [
-                    [
-                        'query'    => 'select * from `tmp`',
-                        'bindings' => [],
-                    ],
-                    [
-                        'offset' => 123,
-                    ],
-                ],
-                'none'           => [
-                    [
-                        'query'    => 'select * from `tmp`',
-                        'bindings' => [],
-                    ],
-                    [
-                        // empty
-                    ],
-                ],
-            ]),
-        ))->getData();
-    }
-
-    /**
-     * @return array<string,mixed>
-     */
-    public function dataProviderHandleScoutBuilder(): array {
-        return [
-            'limit + offset' => [
-                [
-                    'limit'  => 123,
-                    'offset' => 45,
-                ],
-                [
-                    'limit'  => 123,
-                    'offset' => 45,
-                ],
-            ],
-            'limit'          => [
-                [
-                    'limit'  => 123,
-                    'offset' => null,
-                ],
-                [
-                    'limit' => 123,
-                ],
-            ],
-            'offset'         => [
-                [
-                    'limit'  => null,
-                    'offset' => null,
-                ],
-                [
-                    'offset' => 123,
-                ],
-            ],
-            'none'           => [
-                [
-                    'limit'  => null,
-                    'offset' => null,
-                ],
-                [
-                    // empty
-                ],
-            ],
-        ];
-    }
-    // </editor-fold>
+    //</editor-fold>
 }
 
 // @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
