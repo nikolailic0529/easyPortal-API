e6e6427d3817ffc3e6784d817a385957ef80710e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-03 13:55:45 +0400

    New directive: `@orgProperty` (return property value for current organization).

---
+++ app/GraphQL/Directives/Definitions/OrgPropertyDirective.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Directives\Definitions;
+
+use App\GraphQL\Directives\Directives\Org\Property;
+
+class OrgPropertyDirective extends Property {
+    // Lighthouse loads all files from the directives directory...
+}
---
+++ app/GraphQL/Directives/Directives/Org/Property.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Directives\Directives\Org;
+
+use App\Services\Organization\CurrentOrganization;
+use App\Services\Organization\Eloquent\OwnedByOrganizationScope;
+use App\Utils\ModelProperty;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use InvalidArgumentException;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Support\Contracts\ArgBuilderDirective;
+
+use function sprintf;
+
+abstract class Property extends BaseDirective implements ArgBuilderDirective {
+    public function __construct(
+        protected CurrentOrganization $organization,
+    ) {
+        // empty
+    }
+
+    public static function definition(): string {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+            """
+            Return property value for current organization.
+            """
+            directive @orgProperty on FIELD_DEFINITION
+        GRAPHQL;
+    }
+
+    /**
+     * @template T of \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+     *
+     * @param T $builder
+     *
+     * @return T
+     */
+    public function handleBuilder($builder, mixed $value): EloquentBuilder|QueryBuilder {
+        // Query?
+        if (!($builder instanceof EloquentBuilder)) {
+            throw new InvalidArgumentException(sprintf(
+                'Builder must be instance of `%s`, `%s` given.',
+                EloquentBuilder::class,
+                $builder::class,
+            ));
+        }
+
+        // Has scope?
+        /** @var \Illuminate\Database\Eloquent\Model&\App\Services\Organization\Eloquent\OwnedByOrganization $model */
+        $model = $builder->getModel();
+        $scope = OwnedByOrganizationScope::class;
+
+        if (!$model::hasGlobalScope($scope)) {
+            throw new InvalidArgumentException(sprintf(
+                'Model `%s` doesn\'t use the `%s` scope.',
+                $model::class,
+                $scope,
+            ));
+        }
+
+        // Relation?
+        $column   = $model->getOrganizationColumn();
+        $property = new ModelProperty($column);
+        $relation = $property->getRelation($model);
+
+        if (!($relation instanceof BelongsToMany)) {
+            throw new InvalidArgumentException(sprintf(
+                'Relation `%s` is not supported.',
+                $relation::class,
+            ));
+        }
+
+        // Root organization should always use original property
+        if ($this->organization->isRoot()) {
+            return $builder;
+        }
+
+        // Default select?
+        if (!$builder->getQuery()->columns) {
+            $builder = $builder->addSelect($builder->qualifyColumn('*'));
+        }
+
+        // Add property
+        $name    = $this->definitionNode->name->value;
+        $query   = $relation->getQuery()
+            ->select($relation->getQualifiedRelatedPivotKeyName())
+            ->limit(1);
+        $builder = $builder->selectSub($query, $name);
+
+        // Return
+        return $builder;
+    }
+}
