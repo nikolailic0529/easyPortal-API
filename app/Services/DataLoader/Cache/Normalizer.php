8503658c9c63ca10ea7fd872553ba842e98aae6d
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-15 11:17:00 +0400

    `DataLoader`: better json in Cache `Normalizer`.

--- app/Services/DataLoader/Cache/Normalizer.php
+++ app/Services/DataLoader/Cache/Normalizer.php
@@ -6,13 +6,21 @@ use App\Services\DataLoader\Normalizers\KeyNormalizer;
 
 use function json_encode;
 
+use const JSON_THROW_ON_ERROR;
+use const JSON_UNESCAPED_LINE_TERMINATORS;
 use const JSON_UNESCAPED_SLASHES;
 use const JSON_UNESCAPED_UNICODE;
 
 class Normalizer extends KeyNormalizer {
     public function normalize(mixed $value): string {
         $value = parent::normalize($value);
-        $value = (string) json_encode($value, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $value = (string) json_encode(
+            $value,
+            JSON_THROW_ON_ERROR
+            | JSON_UNESCAPED_UNICODE
+            | JSON_UNESCAPED_SLASHES
+            | JSON_UNESCAPED_LINE_TERMINATORS,
+        );
 
         return $value;
     }
