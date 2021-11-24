b321f544be7535aa700cdb771114525851a83b55
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-24 17:35:51 +0400

    [DataLoader] `ResellersRecalculate` added `Documents` to tests.

--- app/Services/DataLoader/Jobs/ResellersRecalculateTest.php
+++ app/Services/DataLoader/Jobs/ResellersRecalculateTest.php
@@ -4,16 +4,22 @@ namespace App\Services\DataLoader\Jobs;
 
 use App\Models\Asset;
 use App\Models\Customer;
+use App\Models\Document;
 use App\Models\Location;
 use App\Models\Reseller;
+use App\Models\ResellerCustomer;
+use App\Models\ResellerLocation;
 use App\Services\DataLoader\Testing\Helper;
 use App\Services\Organization\Eloquent\OwnedByOrganizationScope;
 use App\Utils\Eloquent\GlobalScopes\GlobalScopes;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use LastDragon_ru\LaraASP\Testing\Database\QueryLog\WithQueryLog;
 use Tests\TestCase;
 
 use function count;
+use function in_array;
+use function str_ends_with;
 
 /**
  * @internal
@@ -45,17 +51,20 @@ class ResellersRecalculateTest extends TestCase {
                 'contacts_count'  => $count,
                 'statuses_count'  => $count,
             ]);
-        $resellerB = Reseller::factory()->create([
-            'customers_count' => $count,
-            'locations_count' => $count,
-            'assets_count'    => $count,
-            'contacts_count'  => $count,
-            'statuses_count'  => $count,
-        ]);
+        $resellerB = Reseller::factory()
+            ->hasContacts(1)
+            ->create([
+                'customers_count' => $count,
+                'locations_count' => $count,
+                'assets_count'    => $count,
+                'contacts_count'  => $count,
+                'statuses_count'  => $count,
+            ]);
         $locationA = Location::factory()->create();
         $locationB = Location::factory()->create();
         $customerA = Customer::factory()->create();
         $customerB = Customer::factory()->create();
+        $customerC = Customer::factory()->create();
 
         GlobalScopes::callWithoutGlobalScope(
             OwnedByOrganizationScope::class,
@@ -75,6 +84,10 @@ class ResellersRecalculateTest extends TestCase {
             'customer_id' => $customerB,
             'location_id' => $locationA,
         ]);
+        Document::factory()->create([
+            'reseller_id' => $resellerA,
+            'customer_id' => $customerC,
+        ]);
 
         // Test
         $queries = $this->getQueryLog();
@@ -90,14 +103,26 @@ class ResellersRecalculateTest extends TestCase {
         $this->assertEquals($expected, $actual);
 
         // Properties
+        $customers  = static function (Reseller $reseller): Collection {
+            return ResellerCustomer::query()
+                ->where('reseller_id', '=', $reseller->getKey())
+                ->orderBy('id')
+                ->get();
+        };
+        $locations  = static function (Reseller $reseller): Collection {
+            return ResellerLocation::query()
+                ->where('reseller_id', '=', $reseller->getKey())
+                ->orderBy('id')
+                ->get();
+        };
         $resellerA  = $resellerA->refresh();
+        $customersA = $customers($resellerA);
+        $locationsA = $locations($resellerA);
         $resellerB  = $resellerB->refresh();
+        $customersB = $customers($resellerB);
+        $locationsB = $locations($resellerB);
         $attributes = [
-            'customers_count',
-            'locations_count',
-            'assets_count',
-            'contacts_count',
-            'statuses_count',
+            'customer_id',
         ];
 
         $this->assertEquals([
@@ -106,14 +131,71 @@ class ResellersRecalculateTest extends TestCase {
             'assets_count'    => 2,
             'contacts_count'  => 0,
             'statuses_count'  => 1,
-        ], $resellerA->only($attributes));
+        ], $this->getModelCountableProperties($resellerA, $attributes));
+
+        $this->assertEquals([
+            [
+                'assets_count'    => 0,
+                'locations_count' => 0,
+                'customer_id'     => $customerC->getKey(),
+            ],
+            [
+                'assets_count'    => 1,
+                'locations_count' => 1,
+                'customer_id'     => $customerB->getKey(),
+            ],
+            [
+                'assets_count'    => 1,
+                'locations_count' => 1,
+                'customer_id'     => $customerA->getKey(),
+            ],
+        ], $this->getModelCountableProperties($customersA, $attributes));
+
+        $this->assertEquals([
+            // empty
+        ], $this->getModelCountableProperties($locationsA, $attributes));
 
         $this->assertEquals([
             'customers_count' => 0,
             'locations_count' => 0,
             'assets_count'    => 0,
-            'contacts_count'  => 0,
+            'contacts_count'  => 1,
             'statuses_count'  => 0,
-        ], $resellerB->only($attributes));
+        ], $this->getModelCountableProperties($resellerB, $attributes));
+
+        $this->assertEquals([
+            // empty
+        ], $this->getModelCountableProperties($customersB, $attributes));
+
+        $this->assertEquals([
+            // empty
+        ], $this->getModelCountableProperties($locationsB, $attributes));
+    }
+
+    /**
+     * @param \Illuminate\Support\Collection<\Illuminate\Database\Eloquent\Model>
+     *     |\Illuminate\Database\Eloquent\Model $model
+     * @param array<string> $attributes
+     *
+     * @return array<string, mixed>
+     */
+    protected function getModelCountableProperties(Collection|Model $model, array $attributes = []): array {
+        $properties = [];
+
+        if ($model instanceof Collection) {
+            $properties = $model
+                ->map(function (Model $model) use ($attributes): array {
+                    return $this->getModelCountableProperties($model, $attributes);
+                })
+                ->all();
+        } else {
+            foreach ($model->getAttributes() as $attribute => $value) {
+                if (str_ends_with($attribute, '_count') || in_array($attribute, $attributes, true)) {
+                    $properties[$attribute] = $value;
+                }
+            }
+        }
+
+        return $properties;
     }
 }
