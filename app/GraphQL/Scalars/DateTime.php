32e8a19d00828678adcab2355254685c61cf2b01
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-30 09:40:59 +0400

    [GraphQL] `DateTime` scalar will use new `DateTime` rule to parse dates.

--- app/GraphQL/Scalars/DateTime.php
+++ app/GraphQL/Scalars/DateTime.php
@@ -2,11 +2,9 @@
 
 namespace App\GraphQL\Scalars;
 
-use DateTimeInterface;
+use App\Rules\DateTime as DateTimeRule;
 use Illuminate\Support\Carbon;
 use Nuwave\Lighthouse\Schema\Types\Scalars\DateTimeTz as LighthouseDateTimeTz;
-
-use function app;
 
 /**
  * ISO 8601 Date Time string with format `Y-m-dTH:i:sP` (`2018-05-23T13:43:32+00:00`).
@@ -16,21 +14,9 @@ class DateTime extends LighthouseDateTimeTz {
      * @inheritdoc
      */
     protected function parse($value): Carbon {
-        // Parent method doesn't support DateTimeInterface
-        if ($value instanceof DateTimeInterface) {
-            $value = new Carbon($value);
-        } else {
-            $value = parent::parse($value);
-        }
+        $datetime = (new DateTimeRule())->parse($value);
+        $datetime = Carbon::make($datetime);
 
-        // Set Timezone
-        $tz = app('config')->get('app.timezone') ?: 'UTC';
-
-        if ($value && $tz) {
-            $value = $value->setTimezone($tz);
-        }
-
-        // Return
-        return $value;
+        return $datetime;
     }
 }
