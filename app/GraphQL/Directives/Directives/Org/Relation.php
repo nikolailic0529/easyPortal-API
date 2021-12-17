f141c89913602247503517ca6b5acccf5662373f
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-12-17 15:47:15 +0400

    [GraphQL] Added `@orgRelation` directive that will return value for current organization.

--- app/GraphQL/Directives/Directives/Org/Relation.php
+++ app/GraphQL/Directives/Directives/Org/Relation.php
@@ -3,26 +3,21 @@
 namespace App\GraphQL\Directives\Directives\Org;
 
 use App\Services\Organization\CurrentOrganization;
+use App\Utils\Eloquent\ModelHelper;
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder as QueryBuilder;
-use InvalidArgumentException;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Nuwave\Lighthouse\Execution\BatchLoader\BatchLoaderRegistry;
 use Nuwave\Lighthouse\Execution\BatchLoader\RelationBatchLoader;
 use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
 use Nuwave\Lighthouse\Schema\Directives\RelationDirectiveHelpers;
 use Nuwave\Lighthouse\Schema\Values\FieldValue;
-use Nuwave\Lighthouse\Support\Contracts\ArgBuilderDirective;
 use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 use function array_merge;
-use function sprintf;
 
-// TODO Update property description?
-
-abstract class Property extends BaseDirective implements ArgBuilderDirective, FieldResolver {
+abstract class Relation extends BaseDirective implements FieldResolver {
     use RelationDirectiveHelpers;
 
     public function __construct(
@@ -34,24 +29,36 @@ abstract class Property extends BaseDirective implements ArgBuilderDirective, Fi
     public static function definition(): string {
         return /** @lang GraphQL */ <<<'GRAPHQL'
             """
-            Return property value for current organization.
+            Return relation value for current organization.
             """
-            directive @orgProperty on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+            directive @orgRelation on FIELD_DEFINITION
         GRAPHQL;
     }
 
     public function resolveField(FieldValue $fieldValue): FieldValue {
         $fieldValue->setResolver(
             function (Model $parent, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
+                $name     = $resolveInfo->fieldName;
+                $relation = (new ModelHelper($parent))->getRelation($resolveInfo->fieldName);
+                $property = null;
+
+                if ($relation instanceof BelongsTo) {
+                    $property = $relation->getForeignKeyName();
+                }
+
+                if (!$property) {
+                    return null;
+                }
+
                 /** @var \Nuwave\Lighthouse\Execution\BatchLoader\RelationBatchLoader $batchLoader */
                 $batchLoader = BatchLoaderRegistry::instance(
                     array_merge(
                         $this->qualifyPath($args, $resolveInfo),
                         ["@{$this->name()}"],
                     ),
-                    function () use ($resolveInfo): RelationBatchLoader {
+                    function () use ($property, $name): RelationBatchLoader {
                         return new RelationBatchLoader(
-                            new Loader($this->organization, $resolveInfo->fieldName),
+                            new RelationLoader($this->organization, $name, $property),
                         );
                     },
                 );
@@ -61,34 +68,5 @@ abstract class Property extends BaseDirective implements ArgBuilderDirective, Fi
         );
 
         return $fieldValue;
-    }
-
-    /**
-     * @template T of \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
-     *
-     * @param T $builder
-     *
-     * @return T
-     */
-    public function handleBuilder($builder, mixed $value): EloquentBuilder|QueryBuilder {
-        // Query?
-        if (!($builder instanceof EloquentBuilder)) {
-            throw new InvalidArgumentException(sprintf(
-                'Builder must be instance of `%s`, `%s` given.',
-                EloquentBuilder::class,
-                $builder::class,
-            ));
-        }
-
-        // Add property
-        $name  = $this->nodeName();
-        $query = (new Loader($this->organization, $name))->getQuery($builder);
-
-        if ($query) {
-            $builder = $builder->selectSub($query->limit(1), $name);
-        }
-
-        // Return
-        return $builder;
     }
 }
