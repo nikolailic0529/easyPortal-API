f6647ee0cddab204fbda769dbdc1b5f045475aa1
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-12-29 16:53:07 +0400

    [GraphQL] `org { role { create } }` and `org { role { update } }` mutations will check uniqueness of the `name`.

--- app/GraphQL/Mutations/Org/Role/Builder.php
+++ app/GraphQL/Mutations/Org/Role/Builder.php
@@ -3,10 +3,10 @@
 namespace App\GraphQL\Mutations\Org\Role;
 
 use App\Models\Organization;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
-class Role {
-    public function __invoke(Organization $organization): Builder {
+class Builder {
+    public function __invoke(Organization $organization): EloquentBuilder {
         return $organization->roles()->getQuery();
     }
 }
