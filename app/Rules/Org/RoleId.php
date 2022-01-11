171f9f6f384830d0a82cb8ac67ceed7b65b65036
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-11 12:29:20 +0400

    Rule `OrgRoleId` moved into `Org` namespace.

--- app/Rules/Org/RoleId.php
+++ app/Rules/Org/RoleId.php
@@ -1,21 +1,25 @@
 <?php declare(strict_types = 1);
 
-namespace App\Rules;
+namespace App\Rules\Org;
 
 use App\Models\Role;
 use App\Services\Organization\CurrentOrganization;
 use Illuminate\Contracts\Validation\Rule;
 
 use function __;
-use function app;
 
-class OrgRoleId implements Rule {
+class RoleId implements Rule {
+    public function __construct(
+        protected CurrentOrganization $organization,
+    ) {
+        // empty
+    }
+
     /**
      * @inheritdoc
      */
     public function passes($attribute, $value): bool {
-        $organization = app()->make(CurrentOrganization::class)->get();
-        return $organization
+        return $this->organization->get()
             ->roles()
             ->where((new Role())->getQualifiedKeyName(), '=', $value)
             ->exists();
