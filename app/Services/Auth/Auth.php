6774831fe1f838f652d4e0877091da1235237022
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-20 11:44:22 +0400

    [Auth] Added `Permissions` class that provides list of available permissions (easy for testing).

--- app/Services/Auth/Auth.php
+++ app/Services/Auth/Auth.php
@@ -7,31 +7,7 @@ use App\Models\User;
 use App\Services\Auth\Contracts\Enableable;
 use App\Services\Auth\Contracts\HasPermissions;
 use App\Services\Auth\Contracts\Rootable;
-use App\Services\Auth\Permissions\Administer;
-use App\Services\Auth\Permissions\AssetsDownload;
-use App\Services\Auth\Permissions\AssetsSupport;
-use App\Services\Auth\Permissions\AssetsSync;
-use App\Services\Auth\Permissions\AssetsView;
-use App\Services\Auth\Permissions\ContractsDownload;
-use App\Services\Auth\Permissions\ContractsSupport;
-use App\Services\Auth\Permissions\ContractsSync;
-use App\Services\Auth\Permissions\ContractsView;
-use App\Services\Auth\Permissions\CustomersDownload;
-use App\Services\Auth\Permissions\CustomersSupport;
-use App\Services\Auth\Permissions\CustomersSync;
-use App\Services\Auth\Permissions\CustomersView;
 use App\Services\Auth\Permissions\Markers\IsRoot;
-use App\Services\Auth\Permissions\OrgAdminister;
-use App\Services\Auth\Permissions\QuotesDownload;
-use App\Services\Auth\Permissions\QuotesSupport;
-use App\Services\Auth\Permissions\QuotesSync;
-use App\Services\Auth\Permissions\QuotesView;
-use App\Services\Auth\Permissions\RequestsAssetAdd;
-use App\Services\Auth\Permissions\RequestsAssetChange;
-use App\Services\Auth\Permissions\RequestsContractChange;
-use App\Services\Auth\Permissions\RequestsCustomerChange;
-use App\Services\Auth\Permissions\RequestsQuoteAdd;
-use App\Services\Auth\Permissions\RequestsQuoteChange;
 use App\Services\Organization\CurrentOrganization;
 use App\Services\Organization\RootOrganization;
 use Illuminate\Contracts\Auth\Authenticatable;
@@ -45,6 +21,7 @@ class Auth {
         protected Factory $auth,
         protected RootOrganization $rootOrganization,
         protected CurrentOrganization $currentOrganization,
+        protected Permissions $permissions,
     ) {
         // empty
     }
@@ -77,39 +54,7 @@ class Auth {
      * @return array<\App\Services\Auth\Permission>
      */
     public function getPermissions(): array {
-        return [
-            // Assets
-            new AssetsView(),
-            new AssetsSupport(),
-            new AssetsDownload(),
-            new AssetsSync(),
-            // Contracts
-            new ContractsView(),
-            new ContractsSupport(),
-            new ContractsDownload(),
-            new ContractsSync(),
-            // Customers
-            new CustomersView(),
-            new CustomersSupport(),
-            new CustomersDownload(),
-            new CustomersSync(),
-            // Quotes
-            new QuotesView(),
-            new QuotesSupport(),
-            new QuotesDownload(),
-            new QuotesSync(),
-            // "+ Request" buttons
-            new RequestsAssetAdd(),
-            new RequestsAssetChange(),
-            new RequestsQuoteAdd(),
-            new RequestsQuoteChange(),
-            new RequestsCustomerChange(),
-            new RequestsContractChange(),
-            // Your Organization
-            new OrgAdminister(),
-            // Portal Administration
-            new Administer(),
-        ];
+        return $this->permissions->get();
     }
 
     /**
