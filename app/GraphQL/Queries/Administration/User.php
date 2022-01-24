531b68e0cf23cf7bd0669c9f7b3000e0e45db59e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-24 09:54:10 +0400

    [GraphQL] `User.invitations` will use relation.

--- app/GraphQL/Queries/Administration/User.php
+++ app/GraphQL/Queries/Administration/User.php
@@ -5,22 +5,13 @@ namespace App\GraphQL\Queries\Administration;
 use App\Models\Invitation;
 use App\Models\OrganizationUser;
 use App\Models\Status;
-use App\Models\User as UserModel;
 use App\Services\Organization\CurrentOrganization;
-use Illuminate\Support\Collection;
 
 class User {
     public function __construct(
         protected CurrentOrganization $organization,
     ) {
         // empty
-    }
-
-    public function invitations(UserModel $user): Collection {
-        return $user->invitations()
-            ->where('organization_id', '=', $this->organization->getKey())
-            ->orderBy('created_at', 'desc')
-            ->get();
     }
 
     public function status(OrganizationUser $user): Status {
