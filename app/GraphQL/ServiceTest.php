1a92f88681de621a20bb8b5825aafc79f273c5b8
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-26 16:14:09 +0400

    [GraphQL] Introspection tests moved to `ServiceTest`.

--- app/GraphQL/ServiceTest.php
+++ app/GraphQL/ServiceTest.php
@@ -2,7 +2,16 @@
 
 namespace App\GraphQL;
 
+use App\Models\Enums\UserType;
+use App\Models\User;
 use App\Services\I18n\Locale;
+use Closure;
+use GraphQL\Type\Introspection;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\MergeDataProvider;
+use Tests\GraphQL\GraphQLError;
+use Tests\GraphQL\GraphQLSuccess;
 use Tests\TestCase;
 
 /**
@@ -48,6 +57,22 @@ class ServiceTest extends TestCase {
 
         $this->assertEquals($expected, $this->app->make(Service::class)->isSlowQuery($time));
     }
+
+    /**
+     * @dataProvider dataProviderIntrospection
+     */
+    public function testIntrospection(
+        Response $expected,
+        Closure $settingsFactory,
+        Closure $userFactory,
+    ): void {
+        $this->setSettings($settingsFactory);
+        $this->setUser($userFactory);
+
+        $this
+            ->graphQL(Introspection::getIntrospectionQuery())
+            ->assertThat($expected);
+    }
     // </editor-fold>
 
     // <editor-fold desc="DataProviders">
@@ -62,6 +87,72 @@ class ServiceTest extends TestCase {
             'slow' => [true, 1, 1.034],
             'fast' => [false, 1, 0.034],
         ];
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderIntrospection(): array {
+        $success  = new GraphQLSuccess('__schema', null);
+        $failed   = new GraphQLError('__schema');
+        $enabled  = static function (): array {
+            return [
+                'app.debug' => true,
+            ];
+        };
+        $disabled = static function (): array {
+            return [
+                'app.debug' => false,
+            ];
+        };
+        $guest    = static function (): ?User {
+            return null;
+        };
+        $user     = static function (): ?User {
+            return User::factory()->create();
+        };
+        $root     = static function (): ?User {
+            return User::factory()->create([
+                'type' => UserType::local(),
+            ]);
+        };
+
+        return (new MergeDataProvider([
+            'debug on'  => new ArrayDataProvider([
+                'guest' => [
+                    $success,
+                    $enabled,
+                    $guest,
+                ],
+                'user'  => [
+                    $success,
+                    $enabled,
+                    $user,
+                ],
+                'root'  => [
+                    $success,
+                    $enabled,
+                    $root,
+                ],
+            ]),
+            'debug off' => new ArrayDataProvider([
+                'guest' => [
+                    $failed,
+                    $disabled,
+                    $guest,
+                ],
+                'user'  => [
+                    $failed,
+                    $disabled,
+                    $user,
+                ],
+                'root'  => [
+                    $success,
+                    $disabled,
+                    $root,
+                ],
+            ]),
+        ]))->getData();
     }
     // </editor-fold>
 }
