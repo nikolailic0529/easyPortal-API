f61ed2ff91783196b37fc3f30b000b3cc51b4e3b
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-26 12:27:26 +0400

    GraphQL Playground will be available only in debug mode and for root.

--- app/GraphQL/Provider.php
+++ app/GraphQL/Provider.php
@@ -5,7 +5,10 @@ namespace App\GraphQL;
 use App\Exceptions\GraphQL\ErrorFormatter;
 use App\GraphQL\Directives\Lighthouse\EqDirective;
 use App\GraphQL\Directives\SearchBy\Operators\Complex\Relation as RelationOperator;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators\Complex\Relation as SearchByRelationOperator;
 use Nuwave\Lighthouse\Events\ManipulateResult;
@@ -15,8 +18,9 @@ class Provider extends ServiceProvider {
     /**
      * Bootstrap any application services.
      */
-    public function boot(Dispatcher $dispatcher): void {
+    public function boot(Repository $config, Dispatcher $dispatcher): void {
         $this->bootGraphQL($dispatcher);
+        $this->bootGraphQLPlayground($config);
     }
 
     protected function bootGraphQL(Dispatcher $dispatcher): void {
@@ -29,5 +33,11 @@ class Provider extends ServiceProvider {
                 $event->result->setErrorFormatter($this->app->make(ErrorFormatter::class));
             },
         );
+    }
+
+    protected function bootGraphQLPlayground(Repository $config): void {
+        Gate::define('graphql-playground', static function (?Authenticatable $user) use ($config): bool {
+            return (bool) $config->get('app.debug');
+        });
     }
 }
