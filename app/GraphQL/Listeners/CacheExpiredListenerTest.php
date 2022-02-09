7629be749489e3ee90b0b7824eee416ef2f8fa39
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-02-09 13:48:08 +0400

    GraphQL cache will be marked expired by DataLoader's `DataImported` event.

---
+++ app/GraphQL/Listeners/CacheExpiredListenerTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Listeners;
+
+use App\GraphQL\Service;
+use App\Services\DataLoader\Collector\Data;
+use App\Services\DataLoader\Events\DataImported;
+use Illuminate\Contracts\Events\Dispatcher;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\Services\Search\Listeners\DataImportedListener
+ */
+class CacheExpiredListenerTest extends TestCase {
+    /**
+     * @covers ::subscribe
+     */
+    public function testSubscribe(): void {
+        $this->override(CacheExpiredListener::class, static function (MockInterface $mock): void {
+            $mock
+                ->shouldReceive('__invoke')
+                ->once()
+                ->andReturns();
+        });
+
+        $this->app->make(Dispatcher::class)
+            ->dispatch(new DataImported(new Data()));
+    }
+
+    /**
+     * @covers ::__invoke
+     */
+    public function testInvoke(): void {
+        $this->override(Service::class, static function (MockInterface $mock): void {
+            $mock
+                ->shouldReceive('markCacheExpired')
+                ->once()
+                ->andReturnSelf();
+        });
+
+        $listener = $this->app->make(CacheExpiredListener::class);
+
+        $listener();
+    }
+}
