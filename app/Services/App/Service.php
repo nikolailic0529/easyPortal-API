d524c6dfbe034cf12f78d3a9415762f0f38ec585
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-02-16 11:27:06 +0400

    [App] `Service::getVersion()` will not return `dev-*` strings for version.

--- app/Services/App/Service.php
+++ app/Services/App/Service.php
@@ -5,10 +5,18 @@ namespace App\Services\App;
 use App\Services\Service as BaseService;
 use Composer\InstalledVersions;
 
+use function str_starts_with;
+
 class Service extends BaseService {
     public function getVersion(): ?string {
         $package = InstalledVersions::getRootPackage();
         $version = $package['pretty_version'] ?? null;
+
+        if ($version !== null && str_starts_with($version, 'dev-')) {
+            // Composer may use the branch as a version (`dev-*`), but I'm not
+            // sure how to handle it ... so it is ignored.
+            $version = '0.0.0';
+        }
 
         return $version;
     }
