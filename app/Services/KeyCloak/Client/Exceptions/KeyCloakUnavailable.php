1f2f06490f968073f1d0d7b845ea371241c84ad3
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-02-18 10:55:13 +0400

    [KeyCloak] Fatal errors marked by `IteratorFatalError`.

--- app/Services/KeyCloak/Client/Exceptions/KeyCloakUnavailable.php
+++ app/Services/KeyCloak/Client/Exceptions/KeyCloakUnavailable.php
@@ -3,11 +3,12 @@
 namespace App\Services\KeyCloak\Client\Exceptions;
 
 use App\Exceptions\Contracts\ExternalException;
+use App\Utils\Iterators\Contracts\IteratorFatalError;
 use Throwable;
 
 use function __;
 
-class KeyCloakUnavailable extends ClientException implements ExternalException {
+class KeyCloakUnavailable extends ClientException implements ExternalException, IteratorFatalError {
     public function __construct(Throwable $previous = null) {
         parent::__construct('KeyCloak unavailable.', $previous);
     }
