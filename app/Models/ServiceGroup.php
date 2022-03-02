3ed6046014c47a45780851adb8fb7c4f8f071d50
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-03-02 13:22:03 +0400

    Code cleanup.

--- app/Models/ServiceGroup.php
+++ app/Models/ServiceGroup.php
@@ -8,7 +8,6 @@ use App\Services\I18n\Contracts\Translatable;
 use App\Services\I18n\Eloquent\TranslateProperties;
 use App\Utils\Eloquent\CascadeDeletes\CascadeDelete;
 use App\Utils\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
