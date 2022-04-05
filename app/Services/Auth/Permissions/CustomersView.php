38ac6c573e86728143ef7886d2ea85d8cfe2060a
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-04-05 18:40:21 +0400

    [Auth] `CustomersView` marked as `Composite`.

--- app/Services/Auth/Permissions/CustomersView.php
+++ app/Services/Auth/Permissions/CustomersView.php
@@ -2,10 +2,22 @@
 
 namespace App\Services\Auth\Permissions;
 
+use App\Services\Auth\Contracts\Permissions\Composite;
 use App\Services\Auth\Permission;
 
-final class CustomersView extends Permission {
+final class CustomersView extends Permission implements Composite {
     public function __construct() {
         parent::__construct('customers-view');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPermissions(): array {
+        return [
+            new AssetsView(),
+            new QuotesView(),
+            new ContractsView(),
+        ];
     }
 }
