b4911fc4d87aa2258436eb60b8d05cd0521c36ab
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-05-06 09:05:00 +0400

    [DataLoader] Customers `IteratorImporter`.

---
+++ app/Services/DataLoader/Importer/Importers/Customers/IteratorImporter.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\DataLoader\Importer\Importers\Customers;
+
+use App\Services\DataLoader\Importer\Concerns\WithIterator;
+use App\Services\DataLoader\Schema\Company;
+use App\Utils\Processor\State;
+
+/**
+ * @extends BaseImporter<BaseImporterState>
+ */
+class IteratorImporter extends BaseImporter {
+    /**
+     * @use WithIterator<\App\Models\Customer, Company, BaseImporterState>
+     */
+    use WithIterator;
+
+    protected function getTotal(State $state): ?int {
+        return null;
+    }
+
+    /**
+     * @param BaseImporterState $state
+     *
+     * @return Company|null
+     */
+    protected function getItem(State $state, string $item): mixed {
+        return $this->getClient()->getCustomerById($item);
+    }
+}
