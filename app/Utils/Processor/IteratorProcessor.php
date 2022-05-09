dc312cb75bf53f4126123e9e2a41dc60f0b94b97
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-05-09 09:04:03 +0400

    [Utils/Processor] All processors will run without Telescope, OwnedByOrganizationScope, etc.

--- app/Utils/Processor/IteratorProcessor.php
+++ app/Utils/Processor/IteratorProcessor.php
@@ -2,16 +2,10 @@
 
 namespace App\Utils\Processor;
 
-use App\Services\Organization\Eloquent\OwnedByOrganizationScope;
-use App\Services\Search\Service as SearchService;
-use App\Utils\Eloquent\GlobalScopes\GlobalScopes;
-use App\Utils\Eloquent\SmartSave\BatchSave;
 use App\Utils\Iterators\Concerns\Limit;
 use App\Utils\Iterators\Concerns\Offset;
 use App\Utils\Iterators\Contracts\Limitable;
 use App\Utils\Iterators\Contracts\Offsetable;
-use Closure;
-use Laravel\Telescope\Telescope;
 
 /**
  * The Processor for Iterator.
@@ -25,57 +19,4 @@ use Laravel\Telescope\Telescope;
 abstract class IteratorProcessor extends Processor implements Limitable, Offsetable {
     use Limit;
     use Offset;
-
-    // <editor-fold desc="Process">
-    // =========================================================================
-    /**
-     * @param TState $state
-     */
-    protected function invoke(State $state): void {
-        $this->call(function () use ($state): void {
-            parent::invoke($state);
-        });
-    }
-    //</editor-fold>
-
-    // <editor-fold desc="Helpers">
-    // =========================================================================
-    /**
-     * @template T
-     *
-     * @param Closure(): T $callback
-     *
-     * @return T|null
-     */
-    protected function call(Closure $callback): mixed {
-        $result = null;
-
-        // Organization scope should be disabled because we want to process
-        // all objects.
-        GlobalScopes::callWithoutGlobalScope(
-            OwnedByOrganizationScope::class,
-            static function () use (&$result, $callback): void {
-                // Indexing should be disabled to avoid a lot of queued jobs and
-                // speed up processing.
-
-                SearchService::callWithoutIndexing(static function () use (&$result, $callback): void {
-                    // Telescope should be disabled because it stored all data in memory
-                    // and will dump it only after the job/command/request is finished.
-                    // For long-running jobs, this will lead to huge memory usage
-
-                    Telescope::withoutRecording(static function () use (&$result, $callback): void {
-                        // Processor can create a lot of objects, so will be good to
-                        // group multiple inserts into one.
-
-                        BatchSave::enable(static function () use (&$result, $callback): void {
-                            $result = $callback();
-                        });
-                    });
-                });
-            },
-        );
-
-        return $result;
-    }
-    // </editor-fold>
 }
--- app/Utils/Processor/Processor.php
+++ app/Utils/Processor/Processor.php
@@ -2,6 +2,10 @@
 
 namespace App\Utils\Processor;
 
+use App\Services\Organization\Eloquent\OwnedByOrganizationScope;
+use App\Services\Search\Service as SearchService;
+use App\Utils\Eloquent\GlobalScopes\GlobalScopes;
+use App\Utils\Eloquent\SmartSave\BatchSave;
 use App\Utils\Iterators\Concerns\ChunkSize;
 use App\Utils\Iterators\Contracts\Limitable;
 use App\Utils\Iterators\Contracts\ObjectIterator;
@@ -11,6 +15,7 @@ use App\Utils\Processor\Contracts\StateStore;
 use Closure;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
+use Laravel\Telescope\Telescope;
 use LastDragon_ru\LaraASP\Core\Observer\Dispatcher;
 use LogicException;
 use Throwable;
@@ -127,7 +132,9 @@ abstract class Processor implements ProcessorContract {
         $state         = $this->getState() ?? $this->getDefaultState();
 
         try {
-            $this->invoke($state);
+            $this->call(function () use ($state): void {
+                $this->invoke($state);
+            });
         } catch (Interrupt) {
             // ok
         } finally {
@@ -520,6 +527,47 @@ abstract class Processor implements ProcessorContract {
      */
     protected function restoreState(array $state): State {
         return new State($state);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Helpers">
+    // =========================================================================
+    /**
+     * @template T
+     *
+     * @param Closure(): T $callback
+     *
+     * @return T|null
+     */
+    protected function call(Closure $callback): mixed {
+        $result = null;
+
+        // Organization scope should be disabled because we want to process
+        // all objects.
+        GlobalScopes::callWithoutGlobalScope(
+            OwnedByOrganizationScope::class,
+            static function () use (&$result, $callback): void {
+                // Indexing should be disabled to avoid a lot of queued jobs and
+                // speed up processing.
+
+                SearchService::callWithoutIndexing(static function () use (&$result, $callback): void {
+                    // Telescope should be disabled because it stored all data in memory
+                    // and will dump it only after the job/command/request is finished.
+                    // For long-running jobs, this will lead to huge memory usage
+
+                    Telescope::withoutRecording(static function () use (&$result, $callback): void {
+                        // Processor can create a lot of objects, so will be good to
+                        // group multiple inserts into one.
+
+                        BatchSave::enable(static function () use (&$result, $callback): void {
+                            $result = $callback();
+                        });
+                    });
+                });
+            },
+        );
+
+        return $result;
     }
     // </editor-fold>
 }
