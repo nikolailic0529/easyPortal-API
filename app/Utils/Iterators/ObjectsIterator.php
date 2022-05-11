d45518e6319fa1ef021cb72196c5da5ff1e1d8b7
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-05-11 09:05:29 +0400

    [Utils/Iterators] `ObjectsIterator` will implement `Countable`.

--- app/Utils/Iterators/ObjectsIterator.php
+++ app/Utils/Iterators/ObjectsIterator.php
@@ -3,8 +3,11 @@
 namespace App\Utils\Iterators;
 
 use Closure;
+use Countable;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\Collection;
+
+use function count;
 
 /**
  * @template T
@@ -12,24 +15,31 @@ use Illuminate\Support\Collection;
  *
  * @extends OneChunkOffsetBasedObjectIterator<T, V>
  */
-class ObjectsIterator extends OneChunkOffsetBasedObjectIterator {
+class ObjectsIterator extends OneChunkOffsetBasedObjectIterator implements Countable {
     /**
      * @param Collection<array-key, V>|array<V> $items
      * @param Closure(V):T|null                 $converter
      */
     public function __construct(
         ExceptionHandler $exceptionHandler,
-        Collection|array $items,
+        private Collection|array $items,
         ?Closure $converter = null,
     ) {
         parent::__construct(
             $exceptionHandler,
-            static function () use ($items): array {
-                return $items instanceof Collection
-                    ? $items->all()
-                    : $items;
+            function (): array {
+                return $this->items instanceof Collection
+                    ? $this->items->all()
+                    : $this->items;
             },
             $converter,
         );
     }
+
+    // <editor-fold desc="Countable">
+    // =========================================================================
+    public function count(): int {
+        return count($this->items);
+    }
+    // </editor-fold>
 }
