0f5a16e9c40429f966d439348da3decbbdec00ff
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-05-20 12:50:53 +0400

    `IndexesRebuildTest` will use mocks.

--- app/Services/Search/Commands/IndexesRebuildTest.php
+++ app/Services/Search/Commands/IndexesRebuildTest.php
@@ -3,6 +3,12 @@
 namespace App\Services\Search\Commands;
 
 use App\Models\Customer;
+use App\Services\Search\Processors\ModelsProcessor;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Events\Dispatcher;
+use Mockery;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 /**
@@ -21,6 +27,25 @@ class IndexesRebuildTest extends TestCase {
      * @covers ::__invoke
      */
     public function testInvoke(): void {
+        $this->override(ModelsProcessor::class, static function (): MockInterface {
+            $handler    = Mockery::mock(ExceptionHandler::class);
+            $container  = Mockery::mock(Container::class);
+            $dispatcher = Mockery::mock(Dispatcher::class);
+            $processor  = Mockery::mock(ModelsProcessor::class, [$handler, $dispatcher, $container]);
+            $processor->makePartial();
+            $processor
+                ->shouldReceive('setModels')
+                ->with([Customer::class])
+                ->once()
+                ->andReturnSelf();
+            $processor
+                ->shouldReceive('start')
+                ->once()
+                ->andReturn(true);
+
+            return $processor;
+        });
+
         $this
             ->artisan(
                 'ep:search-indexes-rebuild',
