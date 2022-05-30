02ef92b5bd21bd82cce12b34a876e580d53a8199
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-05-30 11:42:09 +0400

    [Settings] New type `DocumentStatus`.

---
+++ app/Services/Settings/Types/DocumentStatus.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\Settings\Types;
+
+use App\Models\Document;
+use App\Models\Status;
+use App\Utils\Eloquent\Callbacks\GetKey;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\Rule;
+
+class DocumentStatus extends Type {
+    /**
+     * @return Collection<int, Status>
+     */
+    public function getValues(): Collection {
+        return Status::query()
+            ->where('object_type', '=', (new Document())->getMorphClass())
+            ->orderByKey()
+            ->get();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getValidationRules(): array {
+        $ids   = $this->getValues()->map(new GetKey())->all();
+        $rules = [Rule::in($ids)];
+
+        return $rules;
+    }
+}
