b560ea3ed3dab6000d21ea0cfe87193365538879
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-06-22 10:48:54 +0400

    Locales renamed to base name.

---
+++ app/Services/I18n/Migrations/AppTranslationsRename.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\I18n\Migrations;
+
+use App\Services\Filesystem\Disks\AppDisk;
+use App\Services\I18n\Storages\AppTranslations;
+use LastDragon_ru\LaraASP\Migrator\Migrations\RawDataMigration;
+use RuntimeException;
+
+use function sprintf;
+
+abstract class AppTranslationsRename extends RawDataMigration {
+    protected function runRawUp(): void {
+        foreach ($this->getRenameMap() as $from => $to) {
+            $this->rename($from, $to);
+        }
+    }
+
+    protected function runRawDown(): void {
+        foreach ($this->getRenameMap() as $to => $from) {
+            $this->rename($from, $to);
+        }
+    }
+
+    protected function rename(string $from, string $to): void {
+        // No to?
+        $toStorage = $this->getStorage($to);
+
+        if ($toStorage->load()) {
+            return;
+        }
+
+        // No from?
+        $fromStorage = $this->getStorage($from);
+        $fromStrings = $fromStorage->load();
+
+        if (!$fromStrings) {
+            return;
+        }
+
+        // Update
+        $toStorage->save($fromStrings) or throw new RuntimeException(sprintf(
+            'Failed to rename locale `%s` into `%s`.',
+            $to,
+            $from,
+        ));
+
+        $fromStorage->delete(true);
+    }
+
+    protected function getStorage(string $locale): AppTranslations {
+        return new AppTranslations($this->getContainer()->make(AppDisk::class), $locale);
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    abstract protected function getRenameMap(): array;
+}
