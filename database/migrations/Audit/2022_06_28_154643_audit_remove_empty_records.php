3a40445c7f1f3db41c7a779e6502b4360938b040
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-06-28 20:10:28 +0400

    Migration to remove empty audit records.

---
+++ database/migrations/Audit/2022_06_28_154643_audit_remove_empty_records.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+use App\Services\Audit\Auditor;
+use LastDragon_ru\LaraASP\Migrator\Migrations\RawMigration;
+
+return new class() extends RawMigration {
+    /**
+     * The name of the database connection to use.
+     *
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
+     *
+     * @var string|null
+     */
+    protected $connection = Auditor::CONNECTION;
+
+    // Please see the associated SQL files
+};
