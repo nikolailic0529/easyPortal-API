63a26662d080b25eaf80ffdca4ba40b6f396628d
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-08-02 10:10:50 +0400

    [tests] New constraint `XlsxAttachment`.

---
+++ tests/Constraints/Attachments/XlsxAttachment.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Constraints\Attachments;
+
+use Illuminate\Routing\ResponseFactory;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Header;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use PHPUnit\Framework\Constraint\IsEqual;
+use SplFileInfo;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Tests\ResponseTypes\XlsxContentType;
+
+class XlsxAttachment extends Response {
+    public function __construct(string $name, SplFileInfo|string $content = null) {
+        $helper      = new class() extends ResponseFactory {
+            /**
+             * @noinspection PhpMissingParentConstructorInspection
+             * @phpstan-ignore-next-line
+             */
+            public function __construct() {
+                // empty
+            }
+
+            public function fallbackName(mixed $name): string {
+                return parent::fallbackName($name);
+            }
+        };
+        $fallback    = $helper->fallbackName($name);
+        $disposition = HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, $name, $fallback);
+        $constraints = [
+            new XlsxContentType(),
+            new Header('Content-Disposition', [
+                new IsEqual($disposition),
+            ]),
+        ];
+
+        if ($content) {
+            $constraints[] = new XlsxAttachmentContent($content);
+        }
+
+        parent::__construct(...$constraints);
+    }
+}
