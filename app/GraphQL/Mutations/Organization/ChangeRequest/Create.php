703054b8cae6c1f160b511d3f5bbfd40d4fd82bf
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-08-25 09:04:09 +0400

    feat(GraphQL): Added `mutation { organization { changeRequest { create } } }`.

---
+++ app/GraphQL/Mutations/Organization/ChangeRequest/Create.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Mutations\Organization\ChangeRequest;
+
+use App\GraphQL\Mutations\Message\Create as Mutation;
+use App\GraphQL\Objects\MessageInput;
+use App\Models\ChangeRequest;
+use App\Models\Organization;
+
+class Create {
+    public function __construct(
+        protected Mutation $mutation,
+    ) {
+        // empty
+    }
+
+    /**
+     * @param array{input: array<string, mixed>} $args
+     */
+    public function __invoke(Organization $root, array $args): ChangeRequest {
+        $input   = new MessageInput($args['input']);
+        $request = $this->mutation->createRequest($root, $input);
+
+        return $request;
+    }
+}
---
+++ app/GraphQL/Mutations/Organization/ChangeRequest/CreateTest.php
@@ -0,0 +1,242 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Mutations\Organization\ChangeRequest;
+
+use App\GraphQL\Directives\Directives\Mutation\Exceptions\ObjectNotFound;
+use App\Mail\RequestChange;
+use App\Models\Organization;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Mail;
+use LastDragon_ru\LaraASP\Testing\Constraints\Response\Response;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Tests\DataProviders\GraphQL\Organizations\AuthOrgRootDataProvider;
+use Tests\DataProviders\GraphQL\Users\OrgUserDataProvider;
+use Tests\GraphQL\GraphQLError;
+use Tests\GraphQL\GraphQLSuccess;
+use Tests\GraphQL\GraphQLValidationError;
+use Tests\GraphQL\JsonFragment;
+use Tests\TestCase;
+use Tests\WithOrganization;
+use Tests\WithSettings;
+use Tests\WithUser;
+use Throwable;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\GraphQL\Mutations\Organization\ChangeRequest\Create
+ *
+ * @phpstan-import-type OrganizationFactory from WithOrganization
+ * @phpstan-import-type UserFactory from WithUser
+ * @phpstan-import-type SettingsFactory from WithSettings
+ */
+class CreateTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::__invoke
+     *
+     * @dataProvider dataProviderInvoke
+     *
+     * @param OrganizationFactory                                      $orgFactory
+     * @param UserFactory                                              $userFactory
+     * @param SettingsFactory                                          $settings
+     * @param Closure(static, ?Organization, ?User): Organization|null $prepare
+     * @param array<string,mixed>|null                                 $input
+     */
+    public function testInvoke(
+        Response $expected,
+        mixed $orgFactory,
+        mixed $userFactory = null,
+        mixed $settings = null,
+        Closure $prepare = null,
+        array $input = null,
+    ): void {
+        // Prepare
+        $org            = $this->setOrganization($orgFactory);
+        $user           = $this->setUser($userFactory, $org);
+        $organizationId = $this->faker->uuid();
+
+        $this->setSettings($settings);
+
+        Mail::fake();
+
+        if ($prepare) {
+            $organizationId = $prepare($this, $org, $user)->getKey();
+        } elseif ($org) {
+            $organizationId = Organization::factory()->create()->getKey();
+        } else {
+            // empty
+        }
+
+        $input ??= [
+            'subject' => 'subject',
+            'message' => 'change request',
+        ];
+
+        // Test
+        $this
+            ->graphQL(
+            /** @lang GraphQL */
+                <<<'GRAPHQL'
+                mutation test($id: ID!, $input: MessageInput!) {
+                    organization(id: $id) {
+                        changeRequest {
+                            create(input: $input) {
+                                result
+                                changeRequest {
+                                    subject
+                                    message
+                                    from
+                                    to
+                                    cc
+                                    bcc
+                                    user_id
+                                    files {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                GRAPHQL,
+                [
+                    'id'    => $organizationId,
+                    'input' => $input,
+                ],
+            )
+            ->assertThat($expected);
+
+        if ($expected instanceof GraphQLSuccess) {
+            Mail::assertSent(RequestChange::class);
+        }
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderInvoke(): array {
+        $prepare  = static function (TestCase $test, ?Organization $org, ?User $user): Organization {
+            if ($user) {
+                $user->email = 'user@example.com';
+            }
+
+            return Organization::factory()->create();
+        };
+        $settings = [
+            'ep.email_address' => 'test@example.com',
+        ];
+
+        return (new CompositeDataProvider(
+            new AuthOrgRootDataProvider('organization'),
+            new OrgUserDataProvider(
+                'organization',
+                [
+                    'administer',
+                ],
+                'ba3f651c-8183-421f-b1c9-1f1f7058e54d',
+            ),
+            new ArrayDataProvider([
+                'ok'              => [
+                    new GraphQLSuccess(
+                        'organization',
+                        new JsonFragment('changeRequest.create', [
+                            'result'        => true,
+                            'changeRequest' => [
+                                'user_id' => 'ba3f651c-8183-421f-b1c9-1f1f7058e54d',
+                                'subject' => 'subject',
+                                'message' => 'change request',
+                                'from'    => 'user@example.com',
+                                'to'      => ['test@example.com'],
+                                'cc'      => ['cc@example.com'],
+                                'bcc'     => ['bcc@example.com'],
+                                'files'   => [
+                                    [
+                                        'name' => 'documents.csv',
+                                    ],
+                                ],
+                            ],
+                        ]),
+                    ),
+                    $settings,
+                    $prepare,
+                    [
+                        'subject' => 'subject',
+                        'message' => 'change request',
+                        'cc'      => ['cc@example.com'],
+                        'bcc'     => ['bcc@example.com'],
+                        'files'   => [
+                            UploadedFile::fake()->create('documents.csv', 100),
+                        ],
+                    ],
+                ],
+                'Invalid Object'  => [
+                    new GraphQLError('organization', static function (): Throwable {
+                        return new ObjectNotFound((new Organization())->getMorphClass());
+                    }),
+                    $settings,
+                    static function (): Organization {
+                        return Organization::factory()->make();
+                    },
+                    [
+                        'subject' => 'subject',
+                        'message' => 'change request',
+                        'cc'      => ['cc@example.com'],
+                        'bcc'     => ['bcc@example.com'],
+                    ],
+                ],
+                'Invalid subject' => [
+                    new GraphQLValidationError('organization'),
+                    $settings,
+                    $prepare,
+                    [
+                        'subject' => '',
+                        'message' => 'change request',
+                        'cc'      => ['cc@example.com'],
+                        'bcc'     => ['bcc@example.com'],
+                    ],
+                ],
+                'Invalid message' => [
+                    new GraphQLValidationError('organization'),
+                    $settings,
+                    $prepare,
+                    [
+                        'subject' => 'subject',
+                        'message' => '',
+                        'cc'      => ['cc@example.com'],
+                        'bcc'     => ['bcc@example.com'],
+                    ],
+                ],
+                'Invalid cc'      => [
+                    new GraphQLValidationError('organization'),
+                    $settings,
+                    $prepare,
+                    [
+                        'subject' => 'subject',
+                        'message' => 'message',
+                        'cc'      => ['wrong'],
+                        'bcc'     => ['bcc@example.com'],
+                    ],
+                ],
+                'Invalid bcc'     => [
+                    new GraphQLValidationError('organization'),
+                    $settings,
+                    $prepare,
+                    [
+                        'subject' => 'subject',
+                        'message' => 'message',
+                        'cc'      => ['cc@example.com'],
+                        'bcc'     => ['wrong'],
+                    ],
+                ],
+            ]),
+        ))->getData();
+    }
+    // </editor-fold>
+}
