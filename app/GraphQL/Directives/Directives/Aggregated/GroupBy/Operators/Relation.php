0f3eed9711cd621803869655b9da0aa20c585183
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-08-30 16:38:57 +0400

    `@aggregatedGroupByOperatorRelation` implementation.

--- app/GraphQL/Directives/Directives/Aggregated/GroupBy/Operators/Relation.php
+++ app/GraphQL/Directives/Directives/Aggregated/GroupBy/Operators/Relation.php
@@ -2,14 +2,44 @@
 
 namespace App\GraphQL\Directives\Directives\Aggregated\GroupBy\Operators;
 
-use Illuminate\Database\Eloquent\Builder;
+use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Handler;
+use LastDragon_ru\LaraASP\GraphQL\Builder\Property;
+use Nuwave\Lighthouse\Execution\Arguments\Argument;
+use Nuwave\Lighthouse\Execution\Arguments\ArgumentSet;
 
-class Relation extends BaseOperator {
+use function is_array;
+
+class Relation extends AsString {
+    public function __construct(
+        // protected SortByDirective $sortByDirective,
+    ) {
+        parent::__construct();
+    }
+
     public static function getName(): string {
         return 'relation';
     }
 
-    protected function getKeyExpression(Builder $builder, string $column): string {
-        return $builder->getGrammar()->wrap($builder->qualifyColumn($column));
+    public function call(Handler $handler, object $builder, Property $property, Argument $argument): object {
+        // Sort
+        if (is_array($argument->value)) {
+            foreach ($argument->value as $value) {
+                if ($value instanceof ArgumentSet) {
+                    $builder = $handler->handle($builder, $property, $value);
+                }
+            }
+        }
+
+        // Group
+        $name    = $property->getName();
+        $base    = $property->getParent()->getChild("{$name}_id");
+        $builder = parent::call($handler, $builder, $base, $argument);
+
+        // Return
+        return $builder;
+    }
+
+    protected function getKeyDirection(Argument $argument): ?string {
+        return null;
     }
 }
---
+++ app/GraphQL/Directives/Directives/Aggregated/GroupBy/Operators/RelationTest.php
@@ -0,0 +1,158 @@
+<?php declare(strict_types = 1);
+
+namespace App\GraphQL\Directives\Directives\Aggregated\GroupBy\Operators;
+
+use Closure;
+use LastDragon_ru\LaraASP\GraphQL\Builder\Property;
+use LastDragon_ru\LaraASP\GraphQL\SortBy\Directives\Directive;
+use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
+use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
+use Nuwave\Lighthouse\Execution\Arguments\Argument;
+use Tests\DataProviders\Builders\BuilderDataProvider;
+use Tests\DataProviders\Builders\EloquentBuilderDataProvider;
+use Tests\TestCase;
+use Tests\WithGraphQLSchema;
+
+/**
+ * @internal
+ * @coversDefaultClass \App\GraphQL\Directives\Directives\Aggregated\GroupBy\Operators\Relation
+ *
+ * @phpstan-import-type BuilderFactory from BuilderDataProvider
+ */
+class RelationTest extends TestCase {
+    use WithGraphQLSchema;
+
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::call
+     *
+     * @dataProvider dataProviderCall
+     *
+     * @param array{query: string, bindings: array<mixed>} $expected
+     * @param BuilderFactory                               $builderFactory
+     * @param Closure(static): Argument                    $argumentFactory
+     */
+    public function testCall(
+        array $expected,
+        Closure $builderFactory,
+        Property $property,
+        Closure $argumentFactory,
+    ): void {
+        $operator  = $this->app->make(Relation::class);
+        $argument  = $argumentFactory($this);
+        $directive = $this->app->make(Directive::class);
+        $builder   = $builderFactory($this);
+        $builder   = $operator->call($directive, $builder, $property, $argument);
+
+        self::assertDatabaseQueryEquals($expected, $builder);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<mixed>
+     */
+    public function dataProviderCall(): array {
+        $factory = static function (string $direction): Closure {
+            return static function (self $test) use ($direction): Argument {
+                $schema   = (string) $test->printGraphQLSchema(
+                /** @lang GraphQL */
+                    <<<'GRAPHQL'
+                    type Query {
+                        test(input: Test @aggregatedGroupBy): String! @mock
+                    }
+
+                    input Test {
+                        parent: Nested
+                        parent_id: ID!
+                    }
+
+                    input Nested {
+                        b: String
+                    }
+                    GRAPHQL,
+                );
+                $argument = $test->getGraphQLArgument(
+                    '[SortByClauseNested!]',
+                    [
+                        [
+                            'b' => $direction,
+                        ],
+                    ],
+                    $schema,
+                );
+
+                return $argument;
+            };
+        };
+
+        return (new CompositeDataProvider(
+            new EloquentBuilderDataProvider(),
+            new ArrayDataProvider([
+                '`property` asc'  => [
+                    [
+                        'query'    => <<<'SQL'
+                            select
+                                `tmp`.`parent_id` as `key`,
+                                count(*) as `count`
+                            from
+                                `tmp`
+                            group by
+                                `key`
+                            order by
+                                (
+                                    select
+                                        `laravel_reserved_0`.`b`
+                                    from
+                                        `tmp` as `laravel_reserved_0`
+                                    where
+                                        `laravel_reserved_0`.`id` = `tmp`.`parent_id`
+                                    order by
+                                        `laravel_reserved_0`.`b` asc
+                                    limit
+                                        1
+                                ) asc
+                        SQL
+                        ,
+                        'bindings' => [],
+                    ],
+                    new Property('parent'),
+                    $factory('asc'),
+                ],
+                '`property` desc' => [
+                    [
+                        'query'    => <<<'SQL'
+                            select
+                                `tmp`.`parent_id` as `key`,
+                                count(*) as `count`
+                            from
+                                `tmp`
+                            group by
+                                `key`
+                            order by
+                                (
+                                    select
+                                        `laravel_reserved_0`.`b`
+                                    from
+                                        `tmp` as `laravel_reserved_0`
+                                    where
+                                        `laravel_reserved_0`.`id` = `tmp`.`parent_id`
+                                    order by
+                                        `laravel_reserved_0`.`b` desc
+                                    limit
+                                        1
+                                ) desc
+                        SQL
+                        ,
+                        'bindings' => [],
+                    ],
+                    new Property('parent'),
+                    $factory('desc'),
+                ],
+            ]),
+        ))->getData();
+    }
+    //</editor-fold>
+}
