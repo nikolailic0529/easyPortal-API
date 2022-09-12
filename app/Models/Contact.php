7bb8c3f3d76adbbcb94b0febfaaa6c81128bb6bd
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-09-12 10:27:02 +0400

    feat(GraphQL)!: Removed unused `ContactsQuery { assets }`, `ContactsQuery { customers }`.

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 use App\Models\Data\Type;
-use App\Models\Relations\HasAssetsOwners;
-use App\Models\Relations\HasCustomersOwners;
 use App\Models\Relations\HasTypes;
 use App\Utils\Eloquent\Pivot;
 use App\Utils\Eloquent\PolymorphicModel;
@@ -17,19 +15,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 /**
  * Contact.
  *
- * @property string                        $id
- * @property string                        $object_id
- * @property string                        $object_type
- * @property string|null                   $name
- * @property string|null                   $email
- * @property string|null                   $phone_number
- * @property bool|null                     $phone_valid
- * @property CarbonImmutable               $created_at
- * @property CarbonImmutable               $updated_at
- * @property CarbonImmutable|null          $deleted_at
- * @property-read Collection<int,Asset>    $assets
- * @property-read Collection<int,Customer> $customers
- * @property Collection<int,Type>          $types
+ * @property string               $id
+ * @property string               $object_id
+ * @property string               $object_type
+ * @property string|null          $name
+ * @property string|null          $email
+ * @property string|null          $phone_number
+ * @property bool|null            $phone_valid
+ * @property CarbonImmutable      $created_at
+ * @property CarbonImmutable      $updated_at
+ * @property CarbonImmutable|null $deleted_at
+ * @property Collection<int,Type> $types
  * @method static ContactFactory factory(...$parameters)
  * @method static Builder|Contact newModelQuery()
  * @method static Builder|Contact newQuery()
@@ -38,8 +34,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Contact extends PolymorphicModel {
     use HasFactory;
     use HasTypes;
-    use HasCustomersOwners;
-    use HasAssetsOwners;
 
     protected const CASTS = [
         'phone_valid' => 'bool',
