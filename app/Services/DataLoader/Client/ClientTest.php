d958eb054b2486bf760ff6fd517b3c0511743626
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-10-06 16:59:12 +0400

    [DataLoader] `ClientTest::testSchema()` will use `WithGraphQLSchema::assertGraphQLSchemaCompatible()`.

--- app/Services/DataLoader/Client/ClientTest.php
+++ app/Services/DataLoader/Client/ClientTest.php
@@ -4,7 +4,6 @@ namespace App\Services\DataLoader\Client;
 
 use App\Services\DataLoader\Schema\Schema;
 use GraphQL\Utils\BuildClientSchema;
-use LastDragon_ru\LaraASP\GraphQL\Testing\GraphQLExpectedSchema;
 use LastDragon_ru\LaraASP\GraphQL\Testing\Package\SchemaPrinter\TestSettings;
 use Tests\TestCase;
 use Tests\WithGraphQLSchema;
@@ -24,19 +23,23 @@ class ClientTest extends TestCase {
     public function testSchema(): void {
         $client = $this->app->make(Client::class);
 
-        if ($client->isEnabled()) {
-            $into     = $client->getIntrospection();
-            $actual   = BuildClientSchema::build($into);
-            $expected = (new GraphQLExpectedSchema(
-                $this->getTestData(Schema::class)->content('.graphql'),
-            ))
-                ->setSettings(
-                    (new TestSettings())->setPrintUnusedDefinitions(true),
-                );
-
-            self::assertGraphQLSchemaEquals($expected, $actual);
-        } else {
+        if (!$client->isEnabled()) {
             self::markTestSkipped('DataLoader is disabled.');
         }
+
+        $into     = $client->getIntrospection();
+        $actual   = BuildClientSchema::build($into);
+        $actual   = (string) $this
+            ->getGraphQLSchemaPrinter(
+                (new TestSettings())->setPrintUnusedDefinitions(true),
+            )
+            ->printSchema($actual);
+        $expected = $this->getTestData(Schema::class)->file('.graphql');
+
+        self::assertGraphQLSchemaCompatible(
+            $expected,
+            $actual,
+        );
     }
+    // </editor-fold>
 }
