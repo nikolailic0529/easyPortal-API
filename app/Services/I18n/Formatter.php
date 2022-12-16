7ade97a44e48aa86c17d02537a411c01817b5dbf
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-12-16 18:03:24 +0400

    [i18n] `Formatter` will not reflect changes of current Locale and Timezone after creation.

--- app/Services/I18n/Formatter.php
+++ app/Services/I18n/Formatter.php
@@ -2,29 +2,15 @@
 
 namespace App\Services\I18n;
 
-use DateTimeZone;
-use Illuminate\Contracts\Config\Repository;
-use Illuminate\Contracts\Foundation\Application;
-use IntlTimeZone;
 use LastDragon_ru\LaraASP\Formatter\Formatter as LaraASPFormatter;
-use LastDragon_ru\LaraASP\Formatter\PackageTranslator;
 
+/**
+ * The class doesn't reflect changes of locale and timezone after creation. It
+ * is fine for our application. If you need to track locale/timezone changes
+ * you can use {@see CurrentFormatter} instead.
+ *
+ * @see Provider
+ */
 class Formatter extends LaraASPFormatter {
-    public function __construct(
-        Application $application,
-        Repository $config,
-        PackageTranslator $translator,
-        protected CurrentLocale $locale,
-        protected CurrentTimezone $timezone,
-    ) {
-        parent::__construct($application, $config, $translator);
-    }
-
-    protected function getDefaultLocale(): string {
-        return $this->locale->get();
-    }
-
-    protected function getDefaultTimezone(): IntlTimeZone|DateTimeZone|string|null {
-        return $this->timezone->get();
-    }
+    // empty
 }
--- app/Services/I18n/Provider.php
+++ app/Services/I18n/Provider.php
@@ -5,8 +5,23 @@ namespace App\Services\I18n;
 use App\Services\I18n\Commands\LocaleExport;
 use App\Services\I18n\Commands\LocaleImport;
 use App\Utils\Providers\ServiceServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 class Provider extends ServiceServiceProvider {
+    public function register(): void {
+        parent::register();
+
+        // Needed to hold Locale/Timezone
+        $this->app->bind(Formatter::class, static function (Container $container): Formatter {
+            $formatter = $container->make(CurrentFormatter::class);
+            $formatter = $formatter
+                ->forTimezone($formatter->getTimezone())
+                ->forLocale($formatter->getLocale());
+
+            return $formatter;
+        });
+    }
+
     public function boot(): void {
         $this->commands(
             LocaleExport::class,
