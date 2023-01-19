ab2ac87d9bfd88ef561e472abca17b9fe7ee80c8
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-01-19 14:51:39 +0400

    Code cleanup.

--- database/migrations/2023_01_19_103553_search_rebuild_indexes.php
+++ database/migrations/2023_01_19_103553_search_rebuild_indexes.php
@@ -4,6 +4,9 @@ use App\Models\Document;
 use App\Services\Search\Migrations\IndexesRebuild;
 
 return new class() extends IndexesRebuild {
+    /**
+     * @inheritDoc
+     */
     protected function getSearchableModels(): array {
         return [
             Document::class,
